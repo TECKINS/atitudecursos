aa3259028658599d29071f4ad3e80349df399f91
Atitude Cursos <atituderhcursos@gmail.com>
2017-06-19 12:42:15 -0300

    Update cabecalho.php

--- Aluno/cabecalho.php
+++ Aluno/cabecalho.php
@@ -1,6 +1,14 @@
 ﻿<div id="wrapper">
         <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
-            <a class="navbar-brand" href="index.php"><img class="logo" src="images/logo.png"/></a>
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html"><img src="../images/logo.png"/></a> 
+            </div>
             <div style="color: white;padding: 5px 20px 5px 20px;float: right;font-size: 16px;">
 
  <!-- Top Menu Items -->
