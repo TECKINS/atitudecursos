12a9b8bf44924c500f0ca6f32fc864ac8676b0ae
Atitude Cursos <atituderhcursos@gmail.com>
2017-06-19 11:58:18 -0300

    Update index.php

--- Aluno/index.php
+++ Aluno/index.php
@@ -17,6 +17,13 @@ protegePagina();
   ga('send', 'pageview');
 
 </script>
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-9095521793912977",
+    enable_page_level_ads: true
+  });
+</script>
       <meta charset="utf-8" />
       <link rel="shortcut icon" href="../images/ico/favicon.png">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../images/ico/apple-touch-icon-144-precomposed.png">
