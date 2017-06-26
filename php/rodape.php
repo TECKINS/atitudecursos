1f77a9f1e1e642a99bbfa1a59b10035624520297
Atitude Cursos <atituderhcursos@gmail.com>
2017-06-26 11:48:05 -0400

    Add files via upload

---
+++ php/rodape.php
@@ -0,0 +1,59 @@
+<div id="footer">
+			<div class="container">
+				<div id="bar">
+<a href="http://twitter.com/PrimeCursos" class="twitter">Twitter</a>
+<a href="https://www.facebook.com/pages/Prime-Cursos/254050971323116?sk=wall" class="facebook">Facebook</a>
+<a href="https://plus.google.com/104563856806235664286" class="nh googleplus">Google Plus</a>
+<a href="https://www.primecursos.com.br/blog/" class="nh blog">Blog</a>
+<a href="#" class="payment togglepaymentbanner">Formas de Pagamento</a>
+<div id="paymentbannerpopup">
+<a href="#" class="close togglepaymentbanner">Fechar</a>
+<img src="images/bannereducativomp.jpg" data-pagespeed-url-hash="911075389" onload="pagespeed.CriticalImages.checkImageForCriticality(this);"/>
+</div>
+<a href="https://www.google.com/transparencyreport/safebrowsing/diagnostic/index.html#url=www.primecursos.com.br" target="_blank" class="navseg"><img src="img/google-check-navseg.png" alt="Navegação Segura" data-pagespeed-url-hash="3076666018" onload="pagespeed.CriticalImages.checkImageForCriticality(this);"></a>
+</div>
+				<ul><li><p class="title">Sobre</p>
+<p class="text">A Prime Cursos &eacute; uma empresa especializada em ensino &agrave; dist&acirc;ncia e refer&ecirc;ncia no segmento desde 2003. Oferecemos <strong>cursos gr&aacute;tis online</strong> criados por profissionais altamente capacitados e voc&ecirc; ainda tem a op&ccedil;&atilde;o de adquirir o seu certificado ao final do curso.</p>
+</li><li><p class="title">Institucional</p>
+<ul>
+<li><a href="/">Home</a></li>
+<li><a href="cursos/">Todos Cursos</a></li>
+<li><a href="cadastro/">Cadastre-se</a></li>
+<li><a href="como-funciona/">Como funciona</a></li>
+<li><a href="avaliacoes/">Avalia&ccedil;&otilde;es</a></li>
+<li><a href="certificado/">Certificado</a></li>
+<li><a href="pagamento/">Pagamentos</a></li>
+<li><a href="contato/">Fale Conosco</a></li>
+<li><a href="blog/">Depoimentos</a></li>
+<li><a href="faq-perguntas-frequentes/">Perguntas Frequentes</a></li>
+</ul>
+</li><li><p class="title">Categorias de Cursos</p>
+<ul>
+<li><a href="recursos-humanos/"> Recursos Humanos </a></li>
+<li><a href="administracao/"> Administra&ccedil;&atilde;o </a></li>
+<li><a href="meio-ambiente/"> Meio ambiente </a></li>
+<li><a href="comunicacao-e-vendas/"> Comunica&ccedil;&atilde;o e Vendas </a></li>
+<li><a href="saude-amp-bem-estar/"> Sa&uacute;de e Bem Estar </a></li>
+<li><a href="http://concursospublicos.primecursos.com.br/"> Concursos P&uacute;blico </a></li>
+<li><a href="programacao/"> Programa&ccedil;&atilde;o </a></li>
+</ul>
+</li><li><p class="title">Cursos mais Procurados</p>
+<ul>
+<li><a href="ingles-basico/"> Curso de Ingl&ecirc;s </a></li>
+<li><a href="espanhol-basico/"> Curso de Espanhol </a></li>
+<li><a href="digitacao/"> Curso de Digita&ccedil;&atilde;o </a></li>
+<li><a href="maquiagem-basico/"> Curso de Maquiagem </a></li>
+<li><a href="informatica-basica/"> Curso de Inform&aacute;tica </a></li>
+<li><a href="fotografia-digital/"> Curso de Fotografia Digital </a></li>
+<li><a href="vendas-diretas/"> Curso de Vendas Direta </a></li>
+</ul>
+</li><li><p class="title">Artigos</p>
+<ul>
+<li><a href="guia-de-profissoes/">Guia de Profiss&otilde;es</a></li>
+<li><a title="INEP - Instituto Nacional de Estudos e Pesquisas Educacionais An&iacute;sio Teixeira" href="https://www.primecursos.com.br/inep" target="_blank">INEP</a></li>
+<li><a href="https://www.primecursos.com.br/curriculo/">Curr&iacute;culo</a></li>
+<li><a href="https://www.primecursos.com.br/o-que-sao-atividades-complementares/" target="_blank">Atividades Complementares</a></li>
+</ul></li></ul>
+				<a href="http://widenet.com.br" rel="nofollow" style="display:block; position:absolute; bottom:0; right:0; margin:0; padding:5px 8px; color:#0B4585;" target="_blank">WideNet</a>
+			</div>
+		</div>
