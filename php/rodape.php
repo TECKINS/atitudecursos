b80741edb50c3f22e32ebbaa9b153b0e272e4a9e
Atitude Cursos <atituderhcursos@gmail.com>
2017-06-26 13:32:11 -0400

    Update rodape.php

--- php/rodape.php
+++ php/rodape.php
@@ -1,1 +1,53 @@
-<div class="footer-v6" id="footer"> <div class="footer hidden-sm hidden-xs"> <div class="container-pesquisa"> <div class="row margin-left-10 margin-right-10"> <!-- About --> <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12 md-margin-bottom-40 text-justify"> <div class="headline"><h2 style="color: #eee; font-weight: 600;">WR Educacional</h2></div> <p>A WR Educacional trabalha sério para mudar a vida de pessoas especiais como você, com base na eficiência, qualidade, confiabilidade e agilidade de nossos serviços. </p><p>Trabalhamos para constituir uma trajetória sólida com base em valores éticos e morais, com a finalidade de formar cidadãos como você.</p> <p>Somos associados a Associação Brasileira de Educação a Distância (ABED) e temos nossa atuação Autorizada de acordo com a Lei nº 9.394/96 e Decreto Presidencial nº 5.154/04.</p> </div> <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12"> <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 md-margin-bottom-40"> <div class="headline text-center"><h2 style="color: #eee; font-weight: 600;">Algumas Páginas Mais Acessadas</h2></div> </div> <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 md-margin-bottom-40"> <ul class="list-unstyled link-list"> <li class="padding-bottom-5 padding-top-5"><a href="/validacao-certificado" title="Autentique aqui o registro do seu certificado">Autenticar Certificado</a></li> <li class="padding-bottom-5 padding-top-5"><a href="/para-empresas" title="Pacotes de cursos especiais para empresas">Para Empresas</a></li> <li class="padding-bottom-5 padding-top-5"><a href="/seja-um-instrutor" title="Venha fazer parte de nossa equipe de colaboradores">Seja um Instrutor WRE</a></li> <li class="padding-bottom-5 padding-top-5"><a href="/blog" title="Conheça nosso blog com várias dicas de estudos">WR Blog</a></li> <li class="padding-bottom-5 padding-top-5"><a href="/lista-de-cursos" title="Conheça nossos mais de 2 mil cursos disponíveis">Lista de Cursos</a></li> </ul> </div><!--/col-md-3--> <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 md-margin-bottom-40"> <ul class="list-unstyled link-list"> <li class="padding-bottom-5 padding-top-5"><a href="/wre-cursos-rapido-gratis-online-com-certificado" title="Conheça os locais onde pode usar seu Certificado">10 locais para usar o Certificado</a></li> <li class="padding-bottom-5 padding-top-5"><a href="/blog/prova-de-titulo-de-educacao-continuada" title="Veja como aplicar seu Certificado em Educação Continuada">Educação Continuada</a></li> <li class="padding-bottom-5 padding-top-5"><a href="/wre-cursos-online-com-certificado" title="Conheça do 5 principais motivos para fazer um Curso Online">5 Motivos para fazer um Curso Online</a></li> <li class="padding-bottom-5 padding-top-5"><a href="/doacao" title="Você pode apoiar nosso site fazendo uma doação.">Faça uma Doação</a></li> <li class="padding-bottom-5 padding-top-5"><a href="/nosso-certificado" title="Conheça do Modelo do seu Certificado.">Modelo do Certificado</a></li> </ul> </div><!--/col-md-3--> <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 md-margin-bottom-40 text-center"> <span class="color-green" style="font-size: 24px"><i class="glyphicon glyphicon-phone-alt">&nbsp;</i>(35)&nbsp;3331-2274</span> <span class="color-green" style="font-size: 24px"><i class="fa fa-whatsapp">&nbsp;</i>WhatsApp(35)&nbsp;98822-9223</span><br> <span class="color-red" style="font-size: 14px">Segunda a Sexta de 09:00 hs as 21:00 hs e Sábado de 09:00 hs as 17:00 hs</span> </div> </div> <!-- End Latest --> <!-- Address --> <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12 md-margin-bottom-40 endereco"> <div class="headline"><h2 style="color: #eee; font-weight: 600;">Nossos Contatos</h2></div> <address class="md-margin-bottom-40"> <span class="color-orange" style="font-size: 18px"> <b>WR Educacional<br>CNPJ: 26.165.960/0001-03</b></span> <br><br> Rua Érico Veríssimo, 105 - Solar dos Lagos - São Lourenço-MG CEP: 37.470-000 <br><br> <span class="color-green"> Email: <span class="color-orange">atendimento@wreducacional.com.br</span> </span> </address> <ul class="footer-socials list-inline" style="font-size: 24px"> <li> <a href="http://www.facebook.com/share.php?u=http://www.wreducacional.com.br" target="_blank" class="tooltips" data-toggle="tooltip" data-placement="top" title="Facebook WR Educacional" data-original-title="Facebook"> <i class="fa fa-facebook"></i> </a> </li> <li> <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Google Plus"> <i class="fa fa-google-plus"></i> </a> </li> </ul> <a class="orange-btn btn-sm" href="/contato">Fale Conosco</a> </div><!--/col-md-3--> <!-- End Address --> </div> </div> </div><!--/footer--> <div class="copyright"> <div class="container"> <div class="col-lg-12 col-md-12 col-sm-12"> <p> 2015 © WR Educacional. Todos os Direitos Reservados. <a href="/politica-de-privacidade" class="color-green">Política de Privacidade</a> | <a href="/termo-de-servico" class="color-green">Termos de Uso</a> </p> </div> </div> </div><!--/copyright--> </div>
+<div class="footer-v6" id="footer"> 
+  <div class="footer hidden-sm hidden-xs"> 
+    <div class="container-pesquisa"> 
+      <div class="row margin-left-10 margin-right-10"> 
+        <!-- About --> <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12 md-margin-bottom-40 text-justify"> 
+        <div class="headline">
+          <h2 style="color: #eee; font-weight: 600;">Atitude Cursos</h2></div> 
+        <p>Nós da Atitude Cursos somos uma empresa que temos como missão levar educação profissionalizante de qualidade aos quatro cantos do brasil e do mundo por um preço justo.</p>
+        <p>Somos associados a Associação Brasileira de Educação a Distância (ABED) e temos nossa atuação Autorizada de acordo com a Lei nº 9.394/96 e Decreto Presidencial nº 5.154/04.</p> </div> 
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12"> 
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 md-margin-bottom-40"> 
+          <div class="headline text-center">
+            <h2 style="color: #eee; font-weight: 600;">Institucional</h2></div> </div> 
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 md-margin-bottom-40"> 
+            <ul class="list-unstyled link-list"> 
+              <li class="padding-bottom-5 padding-top-5">
+                <a href="/validacao-certificado" title="Autentique aqui o registro do seu certificado">Autenticar Certificado</a></li>
+              <li class="padding-bottom-5 padding-top-5"><a href="/para-empresas" title="Pacotes de cursos especiais para empresas">Para Empresas</a></li> 
+              <li class="padding-bottom-5 padding-top-5"><a href="/seja-um-instrutor" title="Venha fazer parte de nossa equipe de colaboradores">Seja um Instrutor Atitude Cursos</a></li> 
+              <li class="padding-bottom-5 padding-top-5"><a href="/blog" title="Conheça nosso blog com várias dicas de estudos">Atitude Cursos Blog</a></li> 
+              <li class="padding-bottom-5 padding-top-5"><a href="/lista-de-cursos" title="Conheça nossos mais de 2 mil cursos disponíveis">Lista de Cursos</a></li> </ul> </div>
+          <!--/col-md-3--> <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 md-margin-bottom-40"> 
+          <ul class="list-unstyled link-list"> 
+            <li class="padding-bottom-5 padding-top-5">
+              <a href="/cursos-rapido-gratis-online-com-certificado" title="Conheça os locais onde pode usar seu Certificado">10 locais para usar o Certificado</a></li> 
+            <li class="padding-bottom-5 padding-top-5"><a href="/blog/prova-de-titulo-de-educacao-continuada" title="Veja como aplicar seu Certificado em Educação Continuada">Educação Continuada</a></li> 
+            <li class="padding-bottom-5 padding-top-5"><a href="/cursos-online-com-certificado" title="Conheça do 5 principais motivos para fazer um Curso Online">5 Motivos para fazer um Curso Online</a></li> 
+            <li class="padding-bottom-5 padding-top-5"><a href="/doacao" title="Você pode apoiar nosso site fazendo uma doação.">Faça uma Doação</a></li> <li class="padding-bottom-5 padding-top-5"><a href="/nosso-certificado" title="Conheça do Modelo do seu Certificado.">Modelo do Certificado</a></li> </ul> </div><!--/col-md-3--> 
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 md-margin-bottom-40 text-center"> 
+            <span class="color-green" style="font-size: 24px">
+              <i class="glyphicon glyphicon-phone-alt">&nbsp;</i>(91)&nbsp;3754-2307</span>
+            <span class="color-green" style="font-size: 24px">
+              <i class="fa fa-whatsapp">&nbsp;</i>WhatsApp(91)&nbsp;98717-2307</span><br> 
+            <span class="color-red" style="font-size: 14px">Segunda a Sexta de 09:00 hs as 21:00 hs e Sábado de 09:00 hs as 17:00 hs</span> </div> </div> <!-- End Latest --> <!-- Address --> 
+        <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12 md-margin-bottom-40 endereco"> 
+          <div class="headline">
+            <h2 style="color: #eee; font-weight: 600;">Nossos Contatos</h2></div> 
+          <address class="md-margin-bottom-40"> 
+            <span class="color-orange" style="font-size: 18px"> 
+              <b>Atitude Cursos<br>CNPJ: 18.135.450/0001-12</b></span> <br><br>
+            Rua Germano Aranha, Qd. 294 Lt. 03 - Vila dos Cabanos - Barcarena-PA CEP: 68.447-000 <br><br> 
+            <span class="color-green"> Email: 
+              <span class="color-orange">atendimento@atitudecursos.org</span> </span> </address> 
+          <ul class="footer-socials list-inline" style="font-size: 24px"> 
+            <li> <a href="http://www.facebook.com/share.php?u=http://www.atitudecursos.org" target="_blank" class="tooltips" data-toggle="tooltip" data-placement="top" title="Facebook Atitude Cursos" data-original-title="Facebook"> 
+              <i class="fa fa-facebook"></i> </a> </li> <li> <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Google Plus"> 
+            <i class="fa fa-google-plus"></i> </a> </li> </ul> 
+          <a class="orange-btn btn-sm" href="/contato">Fale Conosco</a> </div><!--/col-md-3--> <!-- End Address --> </div> </div> </div><!--/footer--> 
+  <div class="copyright"> 
+    <div class="container"> 
+      <div class="col-lg-12 col-md-12 col-sm-12"> 
+        <p> 2017 © Atitude Cursos. Todos os Direitos Reservados. <a href="/politica-de-privacidade" class="color-green">Política de Privacidade</a> | 
+          <a href="/termos-de-uso" class="color-green">Termos de Uso</a> </p> </div> </div> </div><!--/copyright--> </div>
