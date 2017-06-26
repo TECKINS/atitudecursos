006d5bf816d3508a5969ec5360597c640b0b201a
Atitude Cursos <atituderhcursos@gmail.com>
2017-06-26 15:08:54 -0400

    Update rodape.php

--- php/rodape.php
+++ php/rodape.php
@@ -4,13 +4,13 @@
       <div class="row margin-left-10 margin-right-10"> 
         <!-- About --> <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12 md-margin-bottom-40 text-justify"> 
         <div class="headline">
-          <h2 style="color: #eee; font-weight: 600;">Atitude Cursos</h2></div> 
+          <h2 style="color: #000000; font-weight: 600;">Atitude Cursos</h2></div> 
         <p>Nós da Atitude Cursos somos uma empresa que temos como missão levar educação profissionalizante de qualidade aos quatro cantos do brasil e do mundo por um preço justo.</p>
         <p>Somos associados a Associação Brasileira de Educação a Distância (ABED) e temos nossa atuação Autorizada de acordo com a Lei nº 9.394/96 e Decreto Presidencial nº 5.154/04.</p> </div> 
         <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12"> 
           <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 md-margin-bottom-40"> 
           <div class="headline text-center">
-            <h2 style="color: #; font-weight: 600;">Institucional</h2></div> </div> 
+            <h2 style="color: #000000; font-weight: 600;">Institucional</h2></div> </div> 
           <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 md-margin-bottom-40"> 
             <ul class="list-unstyled link-list"> 
               <li class="padding-bottom-5 padding-top-5">
@@ -27,27 +27,27 @@
             <li class="padding-bottom-5 padding-top-5"><a href="/cursos-online-com-certificado" title="Conheça do 5 principais motivos para fazer um Curso Online">5 Motivos para fazer um Curso Online</a></li> 
             <li class="padding-bottom-5 padding-top-5"><a href="/doacao" title="Você pode apoiar nosso site fazendo uma doação.">Faça uma Doação</a></li> <li class="padding-bottom-5 padding-top-5"><a href="/nosso-certificado" title="Conheça do Modelo do seu Certificado.">Modelo do Certificado</a></li> </ul> </div><!--/col-md-3--> 
           <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 md-margin-bottom-40 text-center"> 
-            <span class="color-green" style="font-size: 24px">
+            <span class="" style="font-size: 24px">
               <i class="glyphicon glyphicon-phone-alt">&nbsp;</i>(91)&nbsp;3754-2307</span>
-            <span class="color-green" style="font-size: 24px">
+            <span class="" style="font-size: 24px">
               <i class="fa fa-whatsapp">&nbsp;</i>WhatsApp(91)&nbsp;98717-2307</span><br> 
             <span class="color-red" style="font-size: 14px">Segunda a Sexta de 09:00 hs as 21:00 hs e Sábado de 09:00 hs as 17:00 hs</span> </div> </div> <!-- End Latest --> <!-- Address --> 
         <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12 md-margin-bottom-40 endereco"> 
           <div class="headline">
-            <h2 style="color: #eee; font-weight: 600;">Nossos Contatos</h2></div> 
+            <h2 style="color: #000000; font-weight: 600;">Nossos Contatos</h2></div> 
           <address class="md-margin-bottom-40"> 
-            <span class="color-orange" style="font-size: 18px"> 
+            <span class="" style="font-size: 18px"> 
               <b>Atitude Cursos<br>CNPJ: 18.135.450/0001-12</b></span> <br><br>
             Rua Germano Aranha, Qd. 294 Lt. 03 - Vila dos Cabanos - Barcarena-PA CEP: 68.447-000 <br><br> 
-            <span class="color-green"> Email: 
-              <span class="color-orange">atendimento@atitudecursos.org</span> </span> </address> 
+            <span class=""> Email: 
+              <span class="">atendimento@atitudecursos.org</span> </span> </address> 
           <ul class="footer-socials list-inline" style="font-size: 24px"> 
             <li> <a href="http://www.facebook.com/share.php?u=http://www.atitudecursos.org" target="_blank" class="tooltips" data-toggle="tooltip" data-placement="top" title="Facebook Atitude Cursos" data-original-title="Facebook"> 
               <i class="fa fa-facebook"></i> </a> </li> <li> <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Google Plus"> 
             <i class="fa fa-google-plus"></i> </a> </li> </ul> 
-          <a class="orange-btn btn-sm" href="/contato">Fale Conosco</a> </div><!--/col-md-3--> <!-- End Address --> </div> </div> </div><!--/footer--> 
+          <a class="btn" href="/contato">Fale Conosco</a> </div><!--/col-md-3--> <!-- End Address --> </div> </div> </div><!--/footer--> 
   <div class="copyright"> 
     <div class="container"> 
       <div class="col-lg-12 col-md-12 col-sm-12"> 
-        <p> 2017 © Atitude Cursos. Todos os Direitos Reservados. <a href="/politica-de-privacidade" class="color-green">Política de Privacidade</a> | 
-          <a href="/termos-de-uso" class="color-green">Termos de Uso</a> </p> </div> </div> </div><!--/copyright--> </div>
+        <p> 2017 © Atitude Cursos. Todos os Direitos Reservados. <a href="/politica-de-privacidade" class="">Política de Privacidade</a> | 
+          <a href="/termos-de-uso" class="">Termos de Uso</a> </p> </div> </div> </div><!--/copyright--> </div>
