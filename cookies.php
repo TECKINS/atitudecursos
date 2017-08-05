0232846b8e6bab9be8294ddb5e4c92d3b622319c
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-05 14:32:52 -0400

    Update cookies.php

--- cookies.php
+++ cookies.php
@@ -2,7 +2,7 @@
     $cookie_name = "user";
     $cookie_value = "John Doe";  
 
-    //bool setcookie ( string $name [, string $value = "mt_rand()" [, int $expire = 0 [, string $path = "/" [, string $domain = ".atitudecursos.org" [, bool $secure = false [, bool $httponly = false ]]]]]] )
+    bool setcookie ( string $name [, string $value = "mt_rand()" [, int $expire = 0 [, string $path = "/" [, string $domain = ".atitudecursos.org" [, bool $secure = false [, bool $httponly = false ]]]]]] )
 
     setcookie("user", "mt_rand()", time() + 3600, '/~cookies/', ".atitudecursos.org");
     
@@ -13,7 +13,7 @@
     }
     
   // Cria o mesmo cookie acima só que irá durar três dias
-    setcookie('usuario', 'rand()', (time() + (3 * 24 * 3600)), /cookies/', ".atitudecursos.org");
+    setcookie('usuario', 'rand()', (time() + (3 * 24 * 3600)), '/cookies/', ".atitudecursos.org");
   
   // set the expiration date to one hour ago
     //setcookie("user", "", time() - 3600);
