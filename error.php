4105f86ee79e4e2eddd058040b19686436ca556b
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-05 20:00:49 -0400

    Update error.php

--- error.php
+++ error.php
@@ -3,7 +3,11 @@ $file=fopen("welcome.txt","r");
 
 if(!file_exists("welcome.txt")) {
   die("File not found");
+  error_function(error_level,error_message,
+error_file,error_line,error_context);
 } else {
   $file=fopen("welcome.txt","r");
 }
+
+
 ?>
