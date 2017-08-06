00f57ac6ffbb1768105d93cf124973567372f435
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-06 08:19:09 -0400

    Update error.php

--- error.php
+++ error.php
@@ -15,14 +15,26 @@ function customError($errno, $errstr) {
 }
 
 //set error handler
-set_error_handler("customError");
+//set_error_handler("customError");
 
 //trigger error
 //echo($test);
 
 $test=2;
+
+//error handler function
+function customError($errno, $errstr) {
+  echo "<b>Error:</b> [$errno] $errstr<br>";
+  echo "Ending Script";
+  die();
+}
+
+//set error handler
+set_error_handler("customError",E_USER_WARNING);
+
+//trigger error
 if ($test>=1) {
-  trigger_error("Value must be 1 or below");
+  trigger_error("Value must be 1 or below",E_USER_WARNING);
 }
 
 
