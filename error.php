b337417d1de1ac7ad2f5cfc817eba3d8c260ba58
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-06 08:09:55 -0400

    Update error.php

--- error.php
+++ error.php
@@ -18,7 +18,12 @@ function customError($errno, $errstr) {
 set_error_handler("customError");
 
 //trigger error
-echo($test);
+//echo($test);
+
+$test=2;
+if ($test>=1) {
+  trigger_error("Value must be 1 or below");
+}
 
 
 ?>
