7e5f4fd460d339f42fdaa9e1663cd96dc34cd7c4
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-06 10:58:39 -0400

    Update error.php

--- error.php
+++ error.php
@@ -21,7 +21,7 @@ error_file,error_line,error_context);
 //echo($test);
 
 
-
+/*
 //error handler function
 function customError($errno, $errstr) {
   echo "<b>Error:</b> [$errno] $errstr<br>";
@@ -37,6 +37,25 @@ $test=2;
 if ($test>=1) {
   trigger_error("Value must be 1 or below",E_USER_WARNING);
 }
+*/
+
+//error handler function
+function customError($errno, $errstr) {
+  echo "<b>Error:</b> [$errno] $errstr<br>";
+  echo "Webmaster has been notified";
+  error_log("Error: [$errno] $errstr",1,
+  "santosdesouzacris@gmail.com","From: webmaster@atitudecursos.com");
+}
+
+//set error handler
+set_error_handler("customError",E_USER_WARNING);
+
+//trigger error
+$test=2;
+if ($test>=1) {
+  trigger_error("Value must be 1 or below",E_USER_WARNING);
+}
+
 
 
 ?>
