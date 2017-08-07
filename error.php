5c6be071fc44b72831b5c6b4c7ccf2eb9f974b5c
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-06 20:41:24 -0400

    Update error.php

--- error.php
+++ error.php
@@ -67,7 +67,7 @@ function checkNum($number) {
 
 //trigger exception
 checkNum(2);
-*/
+
 
 //create function with an exception
 function checkNum($number) {
@@ -87,6 +87,32 @@ try {
 //catch exception
 catch(Exception $e) {
   echo 'Message: ' .$e->getMessage();
+}
+
+*/
+
+class customException extends Exception {
+  public function errorMessage() {
+    //error message
+    $errorMsg = 'Error on line '.$this->getLine().' in '.$this->getFile()
+    .': <b>'.$this->getMessage().'</b> is not a valid E-Mail address';
+    return $errorMsg;
+  }
+}
+
+$email = "someone@example...com";
+
+try {
+  //check if
+  if(filter_var($email, FILTER_VALIDATE_EMAIL) === FALSE) {
+    //throw exception if email is not valid
+    throw new customException($email);
+  }
+}
+
+catch (customException $e) {
+  //display custom message
+  echo $e->errorMessage();
 }
 
 
