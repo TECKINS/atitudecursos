ae13e6e270e4615a1d33a99434eeab162697bef6
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-06 23:51:48 -0400

    Update error.php

--- error.php
+++ error.php
@@ -114,7 +114,7 @@ catch (customException $e) {
   echo $e->errorMessage();
 }
 
-*/
+
   
   class customException extends Exception {
   public function errorMessage() {
@@ -146,7 +146,36 @@ catch (customException $e) {
 catch(Exception $e) {
   echo $e->getMessage();
 }
+*/
 
+class customException extends Exception {
+  public function errorMessage() {
+    //error message
+    $errorMsg = $this->getMessage().' is not a valid E-Mail address.';
+    return $errorMsg;
+  }
+}
+
+$email = "someone@example.com";
+
+try {
+  try {
+    //check for "example" in mail address
+    if(strpos($email, "example") !== FALSE) {
+      //throw exception if email is not valid
+      throw new Exception($email);
+    }
+  }
+  catch(Exception $e) {
+    //re-throw exception
+    throw new customException($email);
+  }
+}
+
+catch (customException $e) {
+  //display custom message
+  echo $e->errorMessage();
+}
 
 
 ?>
