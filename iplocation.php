b9ced4e2fb1508b1cd7c6f7db3fd5fc69da85a9e
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-09 19:33:15 -0300

    Update iplocation.php

--- iplocation.php
+++ iplocation.php
@@ -10,17 +10,13 @@
   "region": "CA",
   "country": "US",
   "phone": 650
-}
+}*/
 
   $ip = $_SERVER['REMOTE_ADDR'];
 $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
-echo $details->city; // -> "Mountain View"*/
+echo $details->city; // -> "Mountain View"
 
-  $.get("https://ipinfo.io/json", function (response) {
-      $("#ip").html("IP: " + response.ip);
-      $("#address").html("Location: " + response.city + ", " + response.region);
-      $("#details").html(JSON.stringify(response, null, 4));
-  }, "jsonp");
+ 
 
   
 ?>
@@ -29,6 +25,13 @@ echo $details->city; // -> "Mountain View"*/
 <html>
 <head>
 <title>IP Location</title>
+  <script>
+     $.get("https://ipinfo.io/json", function (response) {
+      $("#ip").html("IP: " + response.ip);
+      $("#address").html("Location: " + response.city + ", " + response.region);
+      $("#details").html(JSON.stringify(response, null, 4));
+  }, "jsonp");
+  </script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 </head>
 <body>
