c7a4e5b24193903a75b68cd35c990c38a68c6a59
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-10 13:11:12 -0300

    Update maps.php

--- maps.php
+++ maps.php
@@ -1,12 +1,36 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Google Maps</title>
-</head>
-<body>
+  <head>
+      <title>Google Maps</title>
+    <style>
+      html,
+      body {
+        font-family: Arial, sans-serif;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+      #map {
+        height: 100%;
+      }
+    </style>
+  </head>
+  <body>
     <div id="map"></div>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBUrjcYDW8ztOKyw8_OCYl1vQRMVF8XgaQ&callback=initMap"
-    async defer></script>
+    <script>
+      var map;
+      function initMap() {
+        // Constructor creates a new map - only center and zoom are required.
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 40.7413549, lng: -73.9980244},
+          zoom: 13
+        });
+      }
+    </script>
 
-</body>
+    <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=MYAPIKEY&v=3&callback=initMap">
+    </script>
+
+  </body>
 </html>
