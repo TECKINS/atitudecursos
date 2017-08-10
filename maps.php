6db4820a80cb9660f6496dda3128e7aee279d16a
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-10 13:03:37 -0300

    Update maps.php

--- maps.php
+++ maps.php
@@ -4,6 +4,7 @@
 <title>Google Maps</title>
 </head>
 <body>
+    <div id="map"></div>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBUrjcYDW8ztOKyw8_OCYl1vQRMVF8XgaQ&callback=initMap"
     async defer></script>
 
