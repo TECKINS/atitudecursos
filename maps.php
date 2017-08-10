bb88b5bb5a0a2b2c9d8334b176d945eda1cb21be
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-10 12:50:40 -0300

    Create maps.php

---
+++ maps.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Google Maps</title>
+</head>
+<body>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBUrjcYDW8ztOKyw8_OCYl1vQRMVF8XgaQ&callback=initMap"
+    async defer></script>
+
+</body>
+</html>
+Try it Yourself »
