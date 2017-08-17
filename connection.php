421ff202fc717175f0bbc2c99fd0b7a1a64b89c3
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-17 20:07:35 -0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -8,6 +8,11 @@ try {
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
      echo "Connected successfully"; 
+    
+     $sql = "CREATE DATABASE u768531060_myDB";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "Database created successfully<br>";
     }
 catch(PDOException $e)
     {
