6b79cc01bafbbc35f1eebf2305072b94fb31d2cb
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-17 19:52:24 -0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -7,7 +7,16 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=u768531060_users", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
+    // echo "Connected successfully"; 
+    // Create database
+    $sql = "CREATE DATABASE u768531060_myDB";
+    if ($conn->query($sql) === TRUE) {
+        echo "Database created successfully";
+    } else {
+        echo "Error creating database: " . $conn->error;
+    }
+
+    $conn->close();
     }
 catch(PDOException $e)
     {
