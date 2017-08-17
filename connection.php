c8f61b2d82555bc7af1d59c72e3ecd6146c60d6d
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-17 20:03:46 -0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -7,7 +7,7 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=u768531060_users", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected successfully"; 
+     echo "Connected successfully"; 
     }
 catch(PDOException $e)
     {
