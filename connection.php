aab3125522ac6ccc6fe96a8e106b093a09e27c4b
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-17 20:42:25 -0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -14,7 +14,7 @@ try {
     //$conn->exec($sql);
     //echo "Database created successfully<br>";
     
-    // sql to create table
+    /* sql to create table
     $sql = "CREATE TABLE MyGuests (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     firstname VARCHAR(30) NOT NULL,
@@ -26,6 +26,13 @@ try {
     // use exec() because no results are returned
     $conn->exec($sql);
     echo "Table MyGuests created successfully";
+    */
+    
+    $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+    VALUES ('John', 'Doe', 'john@example.com')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
 
     }
 catch(PDOException $e)
