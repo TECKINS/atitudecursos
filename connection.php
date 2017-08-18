071a03ebb8d25d7f59834093945a8a84431ca755
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-17 21:27:08 -0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -34,7 +34,7 @@ try {
     $conn->exec($sql);
     echo "New record created successfully";
     
-    */
+    
     
     $sql = "INSERT INTO MyGuests (firstname, lastname, email)
     VALUES ('John', 'Doe', 'john@example.com')";
@@ -42,11 +42,29 @@ try {
     $conn->exec($sql);
     $last_id = $conn->lastInsertId();
     echo "New record created successfully. Last inserted ID is: " . $last_id;
+    */
+    
+    // begin the transaction
+    $conn->beginTransaction();
+    // our SQL statements
+    $conn->exec("INSERT INTO MyGuests (firstname, lastname, email) 
+    VALUES ('John', 'Doe', 'john@example.com')");
+    $conn->exec("INSERT INTO MyGuests (firstname, lastname, email) 
+    VALUES ('Mary', 'Moe', 'mary@example.com')");
+    $conn->exec("INSERT INTO MyGuests (firstname, lastname, email) 
+    VALUES ('Julie', 'Dooley', 'julie@example.com')");
+
+    // commit the transaction
+    $conn->commit();
+    echo "New records created successfully";
 
     }
 catch(PDOException $e)
     {
-    echo "Connection failed: " . $e->getMessage();
+    // roll back the transaction if something failed
+    $conn->rollback();
+    
+    echo "Error: " . $e->getMessage();
 }
     
 
