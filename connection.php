2a09634a52df09eca4600b3473a3b883e7a239f2
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-18 09:30:45 -0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -4,15 +4,15 @@ $username = "u768531060_root";
 $password = "1000atitudes";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=u768531060_users", $username, $password);
+    $conn = new PDO("mysql:host=$servername", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
      echo "Connected successfully"; 
     
-     //$sql = "CREATE DATABASE u768531060_myDB";
+     $sql = "CREATE DATABASE u768531060_myDB";
     // use exec() because no results are returned
-    //$conn->exec($sql);
-    //echo "Database created successfully<br>";
+    $conn->exec($sql);
+    echo "Database created successfully<br>";
     
     /* sql to create table
     $sql = "CREATE TABLE MyGuests (
@@ -42,7 +42,7 @@ try {
     $conn->exec($sql);
     $last_id = $conn->lastInsertId();
     echo "New record created successfully. Last inserted ID is: " . $last_id;
-    */
+    
     
     // begin the transaction
     $conn->beginTransaction();
@@ -58,6 +58,7 @@ try {
     $conn->commit();
     echo "New records created successfully";
 
+    */
     }
 catch(PDOException $e)
     {
