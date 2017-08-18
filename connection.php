7e2183177032b9e58eb97ab9ae694d7af44341dd
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-17 21:05:18 -0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -26,13 +26,22 @@ try {
     // use exec() because no results are returned
     $conn->exec($sql);
     echo "Table MyGuests created successfully";
-    */
+    
     
     $sql = "INSERT INTO MyGuests (firstname, lastname, email)
     VALUES ('John', 'Doe', 'john@example.com')";
     // use exec() because no results are returned
     $conn->exec($sql);
     echo "New record created successfully";
+    
+    */
+    
+    $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+    VALUES ('John', 'Doe', 'john@example.com')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    $last_id = $conn->lastInsertId();
+    echo "New record created successfully. Last inserted ID is: " . $last_id;
 
     }
 catch(PDOException $e)
