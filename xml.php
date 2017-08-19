dece12e714cef0622f576d950d226c07719f9aab
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-19 10:23:27 -0300

    Update xml.php

--- xml.php
+++ xml.php
@@ -1,4 +1,9 @@
 <?php
-$xml=simplexml_load_file("note.xml") or die("Error: Cannot create object");
-print_r($xml);
+//$xml=simplexml_load_file("note.xml") or die("Error: Cannot create object");
+//print_r($xml);
+
+$xmlDoc = new DOMDocument();
+$xmlDoc->load("note.xml");
+
+print $xmlDoc->saveXML();
 ?>
