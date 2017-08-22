c8940d374cc4b805c149d5986aa5b870ce882b6c
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-22 07:43:32 -0300

    Update iplocation.php

--- iplocation.php
+++ iplocation.php
@@ -222,41 +222,9 @@ $sql = mysqli_query($conexao, "INSERT INTO users (ip, org, loc, city, region, co
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCarvSEZu-SUeDXnENIqhShvjJNZdMBeN0&callback=initMap">
     </script>
             </div>
-		<div id="map"></div>
-    <script>
-      function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCarvSEZu-SUeDXnENIqhShvjJNZdMBeN0&callback=initMap">
-    </script>
+		
         </div>
-	    <div id="map"></div>
-    <script>
-      function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCarvSEZu-SUeDXnENIqhShvjJNZdMBeN0&callback=initMap">
-    </script>
+	   
     </div>
 
     <footer id="footer">
