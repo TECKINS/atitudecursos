302c37069633dfdb737129c7e4c92a903ca3cda8
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-23 08:37:50 -0300

    Update boot.php

--- boot.php
+++ boot.php
@@ -17,6 +17,10 @@
       <div class="col-sm-4">.col-sm-4</div>
       <div class="col-sm-4">.col-sm-4</div>
     </div>
+     <div class="row">
+      <div class="col-sm-4">.col-sm-4</div>
+      <div class="col-sm-8">.col-sm-8</div>
+    </div>
    </div>
   </body>
 </html>
