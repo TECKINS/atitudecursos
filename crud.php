f9cfa3d09c4b2f6c1034514acd6e0e6d5d5e9048
Atitude Cursos <atituderhcursos@gmail.com>
2017-08-27 16:10:31 -0300

    Update crud.php

--- crud.php
+++ crud.php
@@ -1,198 +1,84 @@
 <script>
-//$(document).ready(function(){
-  $(function () {
-
-
-  var input = {};
-  input.CEP = $("input.CEP");
-  input.DN= $("input.CEP");
-
-  var cancelar = $("#cancelar");
-  var nome= $("#nome");
-  var DN= $("#DN");
-  var email= $("#email");
-  var CEP= $("#CEP");
-
-  input.CEP.mask("99.999-999"); 
-  input.DN.mask("99/99/9999");
-
-  cancelar.on('click', function(){
-      nome.val('');
-      DN.val(''); 
-      email.val('');
-      CEP.val('');
-  });
-  
-  $('#registrar').on('click', function(){
-
-    var nomeVal = form1.nome.value;
-    var emailVal = form1.email.value;
-    var ddnVal = form1.DN.value;
-    var cepVal = form1.CEP.value;
-
-    var testCad = 0;
-
-    console.log(nomeVal);
-
-
-
-    if(nomeVal != "" && ddnVal != "" && emailVal != "" && cepVal != ""){
-
-
-
-      var filtro = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-
-      if(filtro.test(emailVal))
-      {
-
-
-        $('#tabCrud').append('<tr"><td>'+ nomeVal +'</td> <td>'+ emailVal +'</td><td>'+ ddnVal +'</td><td>'+ cepVal +'</td> <td>
-                             <input type="button" class="AltBut" value="Alterar"/></td> <td><input type="button" class="salBut" value="Salvar"/></td><td><input type="button" class="ExBut" value="Excluir"/></td></tr>');
-
-        $('#nome').val('');
-        $('#DN').val('');	
-        $('#email').val('');
-        $('#CEP').val('');
-
-        //$('.salBut').hide();
-
-
-        //$(".AltBut").bind("click", Editar);  
-        //$(".salBut").bind("click", FunSal);
-        //$(".ExBut").bind("click", Excluir);
-        
-        var linha = $('<tr><td>'+ nomeVal +'</td> <td>'+ emailVal +'</td><td>'+ ddnVal +'</td><td>'+ cepVal +'</td> <td><input type="button" class="AltBut" value="Alterar"/></td> <td><input type="button" class="salBut" value="Salvar"/></td><td><input type="button" class="ExBut" value="Excluir"/></td></tr>');
-
-        $(".AltBut", linha).on("click", Editar);  
-        $(".salBut", linha).on("click", FunSal);
-        $(".ExBut", linha).on("click", Excluir);
-
-        $("#tabCrud").append(linha);
-
-
-        return true;
-
-
-      } else {
-        alert("Este endereço de email não é válido!");
-        document.form1.email.focus();
-        return false;
+$(document).ready(function() {
+  $('#tblEditavel tbody tr td.editavel').dblclick(function() {
+      if ($('td > input').length > 0) {
+        return;
       }
-
-
-
-    }else{
-
-      alert("Todos os campos são obrigatorios");
-    }
-
-
-
-  });	
-
-
-
-  function Editar(){
-
-    var par = $(this).parent().parent(); //tr
-    var tdNome = par.children("td:nth-child(1)");
-    var tdEmail = par.children("td:nth-child(2)");
-    var tdDN = par.children("td:nth-child(3)");
-    var tdCEP = par.children("td:nth-child(4)");
-
-
-    tdNome.html("<input type ='text' name='txtNome' value='"+tdNome.html()+"'/>");
-    tdEmail.html("<input type='text' name='txtEmail' value='"+tdEmail.html()+"'/>");
-    tdDN.html("<input type='text' name='txtDN' value='"+tdDN.html()+"'/>");
-    tdCEP.html("<input type='text' name='txtDN' value='"+tdCEP.html()+"'/>");
-
-
-    //$('.salBut').show()
-
-  }
-
-
-  function FunSal(){
-    var par = $(this).parent().parent(); //tr
-    var tdNome = par.children("td:nth-child(1)");
-    var tdDN = par.children("td:nth-child(2)");
-    var tdEmail = par.children("td:nth-child(3)");
-    var tdCEP = par.children("td:nth-child(4)");
-
-    tdNome.html(tdNome.children("input[type=text]").val());
-    tdDN.html(tdDN.children("input[type=text]").val());
-    tdEmail.html(tdEmail.children("input[type=text]").val());
-    tdCEP.html(tdCEP.children("input[type=text]").val());
-    //$('.AltBut').show();
-    //$('.salBut').hide();
-
-
-  };
-
-
-  function Excluir(){
-    var par = $(this).parent().parent();
-    //$('.AltBut').show() //tr
-    par.remove();
-  };
+      var conteudoOriginal = $(this).text();
+      var novoElemento = $('<input/>', {
+        type: 'text',
+        value: conteudoOriginal
+      });
+      $(this).html(novoElemento.bind('blur keydown', function(e) {
+        var keyCode = e.which;
+        var conteudoNovo = $(this).val();
+        if (keyCode == 13 && conteudoNovo != '' && conteudoNovo != conteudoOriginal) {
+          var objeto = $(this);
+          $.ajax({
+            type: "POST",
+            url: "#",
+            data: {
+              id: $(this).parents('tr').children().first().text(),
+              campo: $(this).parent().attr('title'),
+              valor: conteudoNovo
+            }, //added this comma here
+            success: function(result) {
+              objeto.parent().html(conteudoNovo);
+              $('body').append(result);
+            }
+          })
+        } else if (keyCode == 27 || e.type == 'blur'){
+          $(this).parent().html(conteudoOriginal);
+          }
+      }));
+      $(this).children().select();
     
-    $("#tabCrud").on("click", ".AltBut", Editar);  
-    $("#tabCrud").on("click", ".salBut", FunSal);
-    $("#tabCrud").on("click", ".ExBut", Excluir);
-});
+        //} removed the extra } from here.
+    });
+
+  })
 </script>
 <html>
   <head>
   <title>CRUD</title>
   <style>
-    body {background-color: #FFE4B5;}
-    table.bordasimples {border-collapse: collapse;}
+    table {
+      border-collapse: collapse;
+    }
+    table,
+    td,
+    th {
+      border: 1px solid black;
+      padding: 5px;
+    }
   </style>
   </head>
 <body>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-  <script type="text/javascript" src="http://files.rafaelwendel.com/jquery.js"></script>
-  <script type="text/javascript" src="http://files.rafaelwendel.com/jquery.maskedinput.js"></script>
-  <form name="form1" action="#" method="post">
-    <table boder = '1px' align=center  >
-      <caption>CADASTRO</caption>
-      <tr>
-        <td><label for ="nome">*Nome: </label></td>
-        <td><input id="nome" type="text" name = "nome"></td>
-        <td><label id="labelNome"></label> </td>
-      </tr>
-      <tr>
-        <td><label for ="Email">*Email: </label></td>
-        <td><input type="text" id="email" name = "email"></td>
-      </tr>
-      <tr>
-        <td><label for ="Data de Nascimento">*Data de Nascimento: </label></td>
-        <td><input type="text" class="DN" id="DN" name = "DN"></td>
-      </tr>
-      <tr>
-      </tr>
-      <tr>
-        <td><label for ="CEP">*CEP: </label></td>
-        <td><input type="text" id="CEP" class="CEP" name = "CEP"></td>
-      </tr>
-      <tr>
-        <td colspan="2">
-          <!--<input type="submit" id="cadastrar" onclick="validar()">-->
-          <input type="button" name="registrar" id="registrar" value="registrar">
-          <input type="button" name="Cancelar" id="cancelar" value="Cancelar">
-        </td>
-      </tr>
-    </table>
-  </form>
-  <div >
-    <table id="tabCrud" border ="1px" align=center bordercolor = " #8B8682  " class="bordasimples" bgcolor =" #D2B48C">
-      <tr>
-        <td>Nome</td>
-        <td>e-mail</td>
-        <td>Data Nascimento</td>
-        <td>CEP</td>
-      </tr>
-    </table>
-  </div>
+<table id="tblEditavel">
+  <thead>
+    <tr>
+      <th>Id</th>
+      <th>Nome</th>
+      <th>Valor</th>
+      <th>Vencimento</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>74</td>
+      <td title="Nome" class="editavel">Conta de Telefone</td>
+      <td title="valor" class="editavel">2.350,00</td>
+      <td title="vencimento" class="editavel">10/02/2017</td>
+    </tr>
+    <tr>
+      <td>74</td>
+      <td title="Nome" class="editavel">Conta de água</td>
+      <td title="valor" class="editavel">235,00</td>
+      <td title="vencimento" class="editavel">18/02/2017</td>
+    </tr>
+
+  </tbody>
+</table>
 </body>
 <html>
