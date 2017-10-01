5b5a3911a7ecd70980abe3fe729b4019705eb9aa
Atitude Cursos <atituderhcursos@gmail.com>
2017-10-01 16:22:43 -0300

    Update cabecalho.php

--- php/cabecalho.php
+++ php/cabecalho.php
@@ -14,6 +14,11 @@
 
  <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
+                    <?php
+                    $sql = mysqli_query($conexao,"SELECT id,remetente,destinatario,message,date FROM mensagem ");
+					
+                    while($valor = mysqli_fetch_array($sql)) { 
+                    ?>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-envelope"></i> <b class="caret"></b></a>
                     <ul class="dropdown-menu message-dropdown">
@@ -24,47 +29,21 @@
                                         <img class="media-object" src="http://placehold.it/50x50" alt="">
                                     </span>
                                     <div class="media-body">
-                                        <h5 class="media-heading"><strong>John Smith</strong>
+                                        <h5 class="media-heading"><strong><?=$valor["remetente"]?></strong>
                                         </h5>
-                                        <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
-                                        <p>Lorem ipsum dolor sit amet, consectetur...</p>
+                                        <p class="small text-muted"><i class="fa fa-clock-o"></i> <?=$valor["date"]?></p>
+                                        <p><?=$valor["message"]?></p>
                                     </div>
                                 </div>
                             </a>
                         </li>
-                        <li class="message-preview">
-                            <a href="#">
-                                <div class="media">
-                                    <span class="pull-left">
-                                        <img class="media-object" src="http://placehold.it/50x50" alt="">
-                                    </span>
-                                    <div class="media-body">
-                                        <h5 class="media-heading"><strong>John Smith</strong>
-                                        </h5>
-                                        <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
-                                        <p>Lorem ipsum dolor sit amet, consectetur...</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="message-preview">
-                            <a href="#">
-                                <div class="media">
-                                    <span class="pull-left">
-                                        <img class="media-object" src="http://placehold.it/50x50" alt="">
-                                    </span>
-                                    <div class="media-body">
-                                        <h5 class="media-heading"><strong>John Smith</strong>
-                                        </h5>
-                                        <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
-                                        <p>Lorem ipsum dolor sit amet, consectetur...</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
+                            <?php
+                            }
+                            ?>
                         <li class="message-footer">
                             <a href="#">Read All New Messages</a>
                         </li>
+                            
                     </ul>
                 </li>
                 <li class="dropdown">
