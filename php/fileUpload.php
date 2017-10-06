33b728de36664685e686d719b9801de5ff69c76c
Atitude Cursos <atituderhcursos@gmail.com>
2017-10-06 07:51:01 -0300

    Update fileUpload.php

--- php/fileUpload.php
+++ php/fileUpload.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>File Upload</title>
-</head>
-<body>
-<form method="post" enctype="multipart/form-data" action="fileUpload.php">
-   Selecione uma imagem: <input name="arquivo" type="file" />
-   <br />
-   <input type="submit" value="Salvar" />
-</form>
-
-</body>
-</html>
-
 <?php
 /******
  * Upload de imagens
