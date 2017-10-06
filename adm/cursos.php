65e3281f60ca5dd0b7f36ca13ecfd5d4489bedd3
Atitude Cursos <atituderhcursos@gmail.com>
2017-10-05 22:28:25 -0300

    Update cursos.php

--- adm/cursos.php
+++ adm/cursos.php
@@ -206,6 +206,7 @@ $total = mysqli_num_rows($dados);
             </div>
          <!-- /. PAGE WRAPPER  -->
         </div>
+	<?php include("../php/rodape.php"); ?>
      <!-- /. WRAPPER  -->
     <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
     <!-- JQUERY SCRIPTS -->
