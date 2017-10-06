c9e7c1768955c3760ebc715beb532bdcb89122e8
Atitude Cursos <atituderhcursos@gmail.com>
2017-10-06 07:51:23 -0300

    Create file.php

---
+++ file.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>File Upload</title>
+</head>
+<body>
+<form method="post" enctype="multipart/form-data" action="fileUpload.php">
+   Selecione uma imagem: <input name="arquivo" type="file" />
+   <br />
+   <input type="submit" value="Salvar" />
+</form>
+
+</body>
+</html>
