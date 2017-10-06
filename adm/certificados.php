8261355ada7f0decc36182bf5a23f105338a67b5
Atitude Cursos <atituderhcursos@gmail.com>
2017-10-05 22:29:01 -0300

    Update certificados.php

--- adm/certificados.php
+++ adm/certificados.php
@@ -201,6 +201,7 @@ $total = mysqli_num_rows($dados);
                     </div>
                 </div>
             </div>
+	<?php include("../php/rodape.php"); ?>
      <!-- /. WRAPPER  -->
     <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
     <!-- JQUERY SCRIPTS -->
