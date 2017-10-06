fd2fbbaca1934cc0fc0bba4fc19fde4818841cf2
Atitude Cursos <atituderhcursos@gmail.com>
2017-10-05 22:26:27 -0300

    Update index.php

--- adm/index.php
+++ adm/index.php
@@ -277,6 +277,7 @@ protegePaginaAdm();
             </div>
          <!-- /. PAGE WRAPPER  -->
         </div>
+	<?php include("../php/rodape.php"); ?>
      <!-- /. WRAPPER  -->
     <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
     <!-- JQUERY SCRIPTS -->
