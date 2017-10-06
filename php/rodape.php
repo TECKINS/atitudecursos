4f44b7be6368141a52d12549adf23542ead1762e
Atitude Cursos <atituderhcursos@gmail.com>
2017-10-05 21:49:39 -0300

    Update rodape.php

--- php/rodape.php
+++ php/rodape.php
@@ -4,25 +4,25 @@
       <div class="row"> 
         <!-- About --> <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12 md-margin-bottom-40 text-justify"> 
         <div class="headline">
-          <h2 style="color: #000000; font-weight: 600;">Atitude Cursos</h2></div> 
-        <p>Nós da Atitude Cursos somos uma empresa que temos como missão levar educação profissionalizante de qualidade aos quatro cantos do brasil e do mundo por um preço justo.</p>
-        <p>Somos associados a Associação Brasileira de Educação a Distância (ABED) e temos nossa atuação Autorizada de acordo com a Lei nº 9.394/96 e Decreto Presidencial nº 5.154/04.</p> </div> 
+          <h2 style="color: #ffffff; font-weight: 600;">Atitude Cursos</h2></div> 
+        <p style="color: #ffffff;">Nós da Atitude Cursos somos uma empresa que temos como missão levar educação profissionalizante de qualidade aos quatro cantos do brasil e do mundo por um preço justo.</p>
+        <p style="color: #ffffff;">Somos associados a Associação Brasileira de Educação a Distância (ABED) e temos nossa atuação Autorizada de acordo com a Lei nº 9.394/96 e Decreto Presidencial nº 5.154/04.</p> </div> 
         <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12"> 
           <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 md-margin-bottom-40"> 
           <div class="headline text-center">
-            <h2 style="color: #000000; font-weight: 600;">Institucional</h2></div> </div> 
+            <h2 style="color: #ffffff; font-weight: 600;">Institucional</h2></div> </div> 
           <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 md-margin-bottom-40"> 
             <ul class="list-unstyled link-list"> 
               <li class="padding-bottom-5 padding-top-5">
-                <a href="/validacao-certificado" title="Autentique aqui o registro do seu certificado">Autenticar Certificado</a></li>
+                <a href="/validacao-certificado" title="Autentique aqui o registro do seu certificado" style="color: #ffffff;">Autenticar Certificado</a></li>
               <li class="padding-bottom-5 padding-top-5"><a href="/para-empresas" title="Pacotes de cursos especiais para empresas">Para Empresas</a></li> 
               <li class="padding-bottom-5 padding-top-5"><a href="/seja-um-instrutor" title="Venha fazer parte de nossa equipe de colaboradores">Seja um Instrutor Atitude Cursos</a></li> 
-              <li class="padding-bottom-5 padding-top-5"><a href="/blog" title="Conheça nosso blog com várias dicas de estudos">Atitude Cursos Blog</a></li> 
+              <li class="padding-bottom-5 padding-top-5" ><a href="/blog" title="Conheça nosso blog com várias dicas de estudos">Atitude Cursos Blog</a></li> 
               <li class="padding-bottom-5 padding-top-5"><a href="/lista-de-cursos" title="Conheça nossos mais de 2 mil cursos disponíveis">Lista de Cursos</a></li> </ul> </div>
           <!--/col-md-3--> <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 md-margin-bottom-40"> 
           <ul class="list-unstyled link-list"> 
             <li class="padding-bottom-5 padding-top-5">
-              <a href="/cursos-rapido-gratis-online-com-certificado" title="Conheça os locais onde pode usar seu Certificado">10 locais para usar o Certificado</a></li> 
+              <a href="/cursos-rapido-gratis-online-com-certificado" title="Conheça os locais onde pode usar seu Certificado" style="color: #ffffff;">10 locais para usar o Certificado</a></li> 
             <li class="padding-bottom-5 padding-top-5"><a href="/blog/prova-de-titulo-de-educacao-continuada" title="Veja como aplicar seu Certificado em Educação Continuada">Educação Continuada</a></li> 
             <li class="padding-bottom-5 padding-top-5"><a href="/cursos-online-com-certificado" title="Conheça do 5 principais motivos para fazer um Curso Online">5 Motivos para fazer um Curso Online</a></li> 
             <li class="padding-bottom-5 padding-top-5"><a href="/doacao" title="Você pode apoiar nosso site fazendo uma doação.">Faça uma Doação</a></li> <li class="padding-bottom-5 padding-top-5"><a href="/nosso-certificado" title="Conheça do Modelo do seu Certificado.">Modelo do Certificado</a></li> </ul> </div><!--/col-md-3--> 
