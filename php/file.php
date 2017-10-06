a0832233e763cf185433a31ef81a35ed929bec3a
Atitude Cursos <atituderhcursos@gmail.com>
2017-10-06 14:49:36 -0300

    Update file.php

--- php/file.php
+++ php/file.php
@@ -1,10 +1,19 @@
+<?php 
+   $destino = '../images/' . $_FILES['arquivo']['name'];
+ 
+   $arquivo_tmp = $_FILES['arquivo']['tmp_name'];
+
+   move_uploaded_file( $arquivo_tmp, $destino  );
+
+   echo "Arquivo enviado com sucesso!";
+?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>File Upload</title>
 </head>
 <body>
-<form method="post" enctype="multipart/form-data" action="fileUpload.php">
+<form method="post" enctype="multipart/form-data" action="file.php">
    Selecione uma imagem: <input name="arquivo" type="file" />
    <br />
    <input type="submit" value="Salvar" />
