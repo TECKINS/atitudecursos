22d64ade24e23b2923fd2a49fa622ebb1ecc1e05
Atitude Cursos <atituderhcursos@gmail.com>
2017-10-07 12:31:06 -0300

    Update form.php

--- Aluno/form.php
+++ Aluno/form.php
@@ -84,6 +84,28 @@ protegePagina();
             <div id="page-inner">
                 <div class="row">
                     <div class="col-md-12">
+			    <!-- breadcrumb -->
+					<ul class="breadcrumb">
+        <li>
+        <i class="fa fa-home"></i>
+        <a href="../index.php">Home</a>
+                    <i class="icon-angle-right"></i>
+            </li>
+
+                            <li>
+                                                            <a href="index.php">
+                            Painel Inicial                       </a>
+                                                                        <i class="icon-angle-right"></i>
+                            </li>
+			    <li>
+                                                            <a href="form.php">
+                            Atendimento                       </a>
+                                                                        <i class="icon-angle-right"></i>
+                            </li>
+                                
+                        
+    </ul>
+	<!-- breadcrumb -->
                      <h2>Atendimento</h2>   
                         <h5><?php echo "Olá <strong>". $_SESSION['nome'] ."</strong>  "?></h5>
                     </div>
