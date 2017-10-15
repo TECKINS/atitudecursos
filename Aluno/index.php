027a8853f81c2fd999fe7b6cd408754d5744b3df
Atitude Cursos <atituderhcursos@gmail.com>
2017-10-15 19:05:38 -0200

    Update index.php

--- Aluno/index.php
+++ Aluno/index.php
@@ -30,7 +30,7 @@ protegePagina();
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../images/ico/apple-touch-icon-114-precomposede.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../images/ico/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed" href="../images/ico/apple-touch-icon-57-precomposed.png">
-    
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Painel Inicial</title>
 	<!-- BOOTSTRAP STYLES-->
@@ -43,43 +43,47 @@ protegePagina();
     <link href="../assets/css/custom.css" rel="stylesheet" />
      <!-- GOOGLE FONTS-->
    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-   
+   <script type="text/javascript">
+     function mudaFoto(foto) {
+       document.getElementById("icone").src = foto;
+     }
+   </script>
 </head>
 <body>
 	<?php include("../php/cabecalho.php"); ?>
-	
+
            <!-- /. NAV TOP  -->
                 <nav class="navbar-default navbar-side" role="navigation">
             <div class="sidebar-collapse">
                 <ul class="nav" id="main-menu">
 				<li class="text-center">
-                    <img src="../assets/img/find_user.png" class="user-image img-responsive"/>
+                    <img id="icone" src="../assets/img/find_user.png" class="user-image img-responsive" width="128px" height="128px"/>
 					</li>
-				
-					
-                    <li>
+
+
+                    <li onmouseover="mudaFoto('../images/painelinicial.png')" onmouseout="mudaFoto('../assets/img/find_user.png')">
                         <a class="active-menu"  href="index.php"><i class="fa fa-dashboard fa-3x"></i> Painel Inicial</a>
                     </li>
-                     <li>
+                     <li onmouseover="mudaFoto('../images/saladeaula.png')" onmouseout="mudaFoto('../assets/img/find_user.png')">
                         <a  href="sala.php"><i class="fa fa-desktop fa-3x"></i> Sala de Aula</a>
                     </li>
-                    <li>
+                    <li onmouseover="mudaFoto('../images/maiscursos.png')" onmouseout="mudaFoto('../assets/img/find_user.png')">
                         <a  href="cursos.php"><i class="fa fa-book fa-3x"></i> Mais Cursos</a>
-                    </li>
-						   <li  >
+                    </li onmouseover="mudaFoto('../images/maiscursos.png')" onmouseout="mudaFoto('../assets/img/find_user.png')">
+						        <li onmouseover="mudaFoto('../images/certificacao.png')" onmouseout="mudaFoto('../assets/img/find_user.png')">
                         <a   href="certificados.php"><i class="fa fa-qrcode fa-3x"></i> Certificados</a>
-                    </li>	
-                      <li  >
+                    </li>
+                      <li onmouseover="mudaFoto('../images/dadosdoaluno.png')" onmouseout="mudaFoto('../assets/img/find_user.png')">
                         <a  href="dados.php"><i class="fa fa-table fa-3x"></i> Dados do Aluno </a>
                     </li>
-                    <li  >
+                    <li onmouseover="mudaFoto('../images/atendimento.png')" onmouseout="mudaFoto('../assets/img/find_user.png')">
                         <a  href="form.php"><i class="fa fa-pencil-square fa-3x"></i> Atendimento </a>
-                    </li>					
+                    </li>
                 </ul>
-               
+
             </div>
-            
-        </nav>  
+
+        </nav>
         <!-- /. NAV SIDE  -->
         <div id="page-wrapper" >
             <div id="page-inner">
@@ -97,18 +101,18 @@ protegePagina();
                                                             <a href="index.php">
                             Painel Inicial                       </a>
                                                                         <i class="icon-angle-right"></i>
-                            </li>                         
-                        
+                            </li>
+
     </ul>
 	<!-- breadcrumb -->
-                     <h2>Área do Aluno</h2>   
+                     <h2>Área do Aluno</h2>
                         <h5><?php echo "Bem vindo <strong>". $_SESSION['nome'] ."</strong>  "?></h5>
                     </div>
-                </div>              
+                </div>
                  <!-- /. ROW  -->
                   <hr />
                 <div class="row">
-                <div class="col-md-4 col-sm-12 col-xs-12">           
+                <div class="col-md-4 col-sm-12 col-xs-12">
 			<div class="panel panel-back noti-box">
                 <span>
                     <i class="icon-box bg-color-red set-icon fa fa-laptop"></i>
@@ -119,7 +123,7 @@ protegePagina();
                 </div>
              </div>
 		     </div>
-                    <div class="col-md-4 col-sm-12 col-xs-12">           
+                    <div class="col-md-4 col-sm-12 col-xs-12">
 			<div class="panel panel-back noti-box">
                 <span >
                     <i class="icon-box bg-color-green set-icon fa fa-gears"></i>
@@ -130,7 +134,7 @@ protegePagina();
                 </div>
              </div>
 		     </div>
-                    <div class="col-md-4 col-sm-12 col-xs-12">           
+                    <div class="col-md-4 col-sm-12 col-xs-12">
 			<div class="panel panel-back noti-box">
                 <span>
                     <i class="icon-box bg-color-blue set-icon fa fa-mortar-board"></i>
@@ -141,12 +145,12 @@ protegePagina();
                 </div>
              </div>
 		     </div>
-                    
+
 			</div>
                  <!-- /. ROW  -->
-                <hr />                
+                <hr />
                 <div class="row">
-                    <div class="col-md-6 col-sm-12 col-xs-12">           
+                    <div class="col-md-6 col-sm-12 col-xs-12">
 			<div class="panel panel-back noti-box">
                 <span>
                     <i class="icon-box bg-color-blue fa fa-info"></i>
@@ -158,44 +162,44 @@ protegePagina();
                     <p class="text-muted">
                           <span class="text-muted color-bottom-txt"><i class="fa fa-pencil-square-o"></i>
                                Uma a duas horas por dia é o tempo ideal para você aprender com
-                               agilidade e eficiência. Sempre fazendo uma pausa a cada meia hora passada de estudos. 
+                               agilidade e eficiência. Sempre fazendo uma pausa a cada meia hora passada de estudos.
                                </span>
                     </p>
                 </div>
              </div>
 		     </div>
-                    
-                    
+
+
                     <div class="col-md-6 col-sm-12 col-xs-12">
                         <div class="panel back-dash">
                                <i class="fa fa-shield fa-3x"></i><strong> &nbsp; Autenticação do Certificado</strong>
-                             <p class="text-muted">Seu certificado possui um número unico, que se encontra na parte de trás do 
+                             <p class="text-muted">Seu certificado possui um número unico, que se encontra na parte de trás do
                              certificado, no canto inferior esquerdo, com a indicação <b>número do certificado</b> (Nº).<br>
                              Teste a validade do seu certificado clicando no botão abaixo.</p>
                              <a href="../certificado.html"><button type="submit" class="btn btn-default">Autenticação do Certificado</button></a>
                         </div>
-                       
+
                     </div>
-                    
-                        
+
+
         </div>
                  <!-- /. ROW  -->
-                <div class="row"> 
-                    
-                      
-                               <div class="col-md-9 col-sm-12 col-xs-12">                     
+                <div class="row">
+
+
+                               <div class="col-md-9 col-sm-12 col-xs-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Introdução a Plataforma da Atidude Cursos
                         </div>
-                        
+
                             <video class="col-md-12 col-sm-12 col-xs-12" width="745" height="390" poster="../assets/intro/capa.png" controls>
                             <source src="../assets/intro/intro.mp4" type="video/mp4">
                             </video>
-                        
-                    </div>            
+
+                    </div>
                 </div>
-                    <div class="col-md-3 col-sm-12 col-xs-12">                       
+                    <div class="col-md-3 col-sm-12 col-xs-12">
                     <div class="panel panel-primary text-center no-boder bg-color-green">
                         <div class="panel-body">
                             <i class="fa fa-bar-chart-o fa-5x"></i>
@@ -203,7 +207,7 @@ protegePagina();
                         </div>
                         <div class="panel-footer back-footer-green">
                            Acompanhamento por indicadores de desempenho
-                            
+
                         </div>
                     </div>
                     <div class="panel panel-primary text-center no-boder bg-color-red">
@@ -213,11 +217,11 @@ protegePagina();
                         </div>
                         <div class="panel-footer back-footer-red">
                             Edite suas informaçoes de contato
-                            
+
                         </div>
-                    </div>                         
+                    </div>
                         </div>
-                
+
            </div>
                  <!-- /. ROW  -->
                 <div class="row" >
@@ -229,14 +233,14 @@ protegePagina();
                              <h4>Membros</h4>
                         </div>
                         <div class="panel-footer back-footer-green">
-                             
+
                             Compartilhe conhecimento no forum e chat da comunidade.
-                            
+
                         </div>
                     </div>
                     </div>
                     <div class="col-md-9 col-sm-12 col-xs-12">
-               
+
                     <div class="panel panel-default">
                         <div class="panel-heading">
                            Tabela de Professores
@@ -290,12 +294,12 @@ protegePagina();
 
                     </div>
                 </div>
-		    
+
     </div>
-		
+
              <!-- /. PAGE INNER  -->
             </div>
-	
+
          <!-- /. PAGE WRAPPER  -->
         </div>
 	<?php include("../php/rodape.php"); ?>
@@ -312,7 +316,7 @@ protegePagina();
     <script src="../assets/js/morris/morris.js"></script>
       <!-- CUSTOM SCRIPTS -->
     <script src="../assets/js/custom.js"></script>
-    
-   
+
+
 </body>
 </html>
