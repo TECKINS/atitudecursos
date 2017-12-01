56a96ba79df3c0397eadbe0641fb431c3d825a15
Atitude Cursos <atituderhcursos@gmail.com>
2017-12-01 15:17:51 -0200

    Update certificados.php

--- adm/certificados.php
+++ adm/certificados.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 session_start();
 
 include("../php/config.php");
@@ -16,225 +16,230 @@ $total = mysqli_num_rows($dados);
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-88350404-1', 'auto');
-  ga('send', 'pageview');
-</script>
-	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-  (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-9095521793912977",
-    enable_page_level_ads: true
-  });
-</script>
-      <meta charset="utf-8" />
-      <link rel="shortcut icon" href="../images/ico/favicon.png">
+  <head>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                              })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-88350404-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+      (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-9095521793912977",
+        enable_page_level_ads: true
+      });
+    </script>
+    <meta charset="utf-8" />
+    <link rel="shortcut icon" href="../images/ico/favicon.png">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../images/ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../images/ico/apple-touch-icon-114-precomposede.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../images/ico/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed" href="../images/ico/apple-touch-icon-57-precomposed.png">
-    
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Certificados</title>
-	<!-- BOOTSTRAP STYLES-->
+    <!-- BOOTSTRAP STYLES-->
     <link href="../assets/css/bootstrap.css" rel="stylesheet"/>
-     <!-- FONTAWESOME STYLES-->
+    <!-- FONTAWESOME STYLES-->
     <link href="../assets/css/font-awesome.css" rel="stylesheet" />
-     <!-- MORRIS CHART STYLES-->
+    <!-- MORRIS CHART STYLES-->
     <link href="../assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
-        <!-- CUSTOM STYLES-->
+    <!-- CUSTOM STYLES-->
     <link href="../assets/css/custom.css" rel="stylesheet" />
-     <!-- GOOGLE FONTS-->
-   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-   
-</head>
-<body>
-	<?php include("../php/cabecalho.php"); ?>
-	<!-- /. NAV TOP  -->
-                <nav class="navbar-default navbar-side" role="navigation">
-            <div class="sidebar-collapse">
-                <ul class="nav" id="main-menu">
-				<li class="text-center">
-                    <img src="../assets/img/find_user.png" class="user-image img-responsive"/>
-					</li>
-				
-					
-                    <li>
-                        <a  href="index.php"><i class="fa fa-dashboard fa-3x"></i> Painel Inicial</a>
-                    </li>
-                     <li>
-                        <a  href="sala.php"><i class="fa fa-desktop fa-3x"></i> Sala de Aula</a>
-                    </li>
-                    <li>
-                        <a  href="cursos.php"><i class="fa fa-book fa-3x"></i> Mais Cursos</a>
-                    </li>
-						   <li  >
-                        <a class="active-menu" href="certificados.php"><i class="fa fa-qrcode fa-3x"></i> Certificados</a>
-                    </li>	
-                      <li  >
-                        <a  href="dados.php"><i class="fa fa-table fa-3x"></i> Dados do Aluno </a>
-                    </li>
-                    <li  >
-                        <a href="form.php"><i class="fa fa-pencil-square fa-3x"></i> Atendimento </a>
-                    </li>					
-                </ul>
-               
+    <!-- GOOGLE FONTS-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+  </head>
+  <body>
+    <?php include("../php/cabecalho.php"); ?>
+    <!-- /. NAV TOP  -->
+    <nav class="navbar-default navbar-side" role="navigation">
+      <div class="sidebar-collapse">
+        <ul class="nav" id="main-menu">
+          <li class="text-center">
+            <img src="../assets/img/find_user.png" class="user-image img-responsive"/>
+          </li>
+
+
+          <li>
+            <a  href="index.php"><i class="fa fa-dashboard fa-3x"></i> Painel Inicial</a>
+          </li>
+          <li>
+            <a  href="sala.php"><i class="fa fa-desktop fa-3x"></i> Sala de Aula</a>
+          </li>
+          <li>
+            <a  href="cursos.php"><i class="fa fa-book fa-3x"></i> Mais Cursos</a>
+          </li>
+          <li  >
+            <a class="active-menu" href="certificados.php"><i class="fa fa-qrcode fa-3x"></i> Certificados</a>
+          </li>	
+          <li  >
+            <a  href="dados.php"><i class="fa fa-table fa-3x"></i> Dados do Aluno </a>
+          </li>
+          <li  >
+            <a href="form.php"><i class="fa fa-pencil-square fa-3x"></i> Atendimento </a>
+          </li>					
+        </ul>
+
+      </div>
+
+    </nav>  
+    <!-- /. NAV SIDE  -->
+    <div id="page-wrapper" >
+      <div id="page-inner">
+      
+
+        <!-- Content Section -->
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <h1>Demo: PHP and MySQL CRUD Operations using Jquery</h1>
             </div>
-            
-        </nav>  
-        <!-- /. NAV SIDE  -->
-        <div id="page-wrapper" >
-            <div id="page-inner">
-                <div class="row">
-                    <div class="col-md-12">
-			    <!-- breadcrumb -->
-					<ul class="breadcrumb">
-        <li>
-        <i class="fa fa-home"></i>
-        <a href="../index.php">Home</a>
-                    <i class="icon-angle-right"></i>
-            </li>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="pull-right">
+                <button class="btn btn-success" data-toggle="modal" data-target="#add_new_record_modal">Novo certificado</button>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <h3>Certificados:</h3>
 
-                            <li>
-                                                            <a href="index.php">
-                            Painel Inicial                       </a>
-                                                                        <i class="icon-angle-right"></i>
-                            </li>
-			    <li>
-                                                            <a href="certificados.php">
-                            Certificados                       </a>
-                                                                        <i class="icon-angle-right"></i>
-                            </li>
-                                
-                        
-    </ul>
-	<!-- breadcrumb -->
-                     <h2>CADASTRAMENTO DE CERTIFICADOS</h2>   
-                        <h5><?php echo "Olá <strong>". $_SESSION['nome'] ."</strong>  "?></h5>
-                    </div>
-                </div>              
-                 <!-- /. ROW  -->
-                 <hr />
-               <div id="content">
-                <div class="row">
+              <div class="records_content"></div>
+            </div>
+          </div>
+        </div>
+        <!-- /Content Section -->
 
-                        <!-- Main Column -->
-                        <div class="col-md-8 down-20">
-                                <div class="col-md-10">
-                                <!-- Main Content -->
-                                <h2>Preencha os campos abaixo</h2>
-                                <div class="status alert alert-success" style="display: none"></div>
-                                <br>
-                            </div>
-                            <!-- Contact Form -->
-                            <div class="col-md-8">
-                            <form name="contact-form" method="post" action="../php/cadcertificados.php">
-                                <label class="mini">Matricula</label>
-                                <div class="row">
-                                    <input name="keyword" class="form-control" type="text">
-                                </div>
-                                <label class="mini">Curso</label>
-                                <div class="row">
-                                    <input name="course" class="form-control" type="text">
-                                </div>
-                                <label class="mini">Descrição</label>
-                                <div class="row">
-                                    <textarea name="description" rows="8" class="form-control">Concluiu com bom aproveitamento</textarea>
-                                </div>
-                                <label class="mini">Data de Inicio</label>
-                                <div class="row">
-                                    <input type="date" name="datainicio" class="form-control">
-                                </div>
-                                <label class="mini">Data de Termino</label>
-                                <div class="row">
-                                    <input type="date" name="datafim" class="form-control">
-                                </div>
-                                <label class="mini">Nome Completo</label>
-                                <div class="row">
-                                    <input name="address" onFocus="" class="form-control" type="text">
-                                </div>
-                                <p>
-                                    <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-send"></span> Enviar</button>
-                                </p>
-                            </form>
-                            <!-- End Contact Form -->
-                            <!-- End Main Content -->
-                        </div>
-                        </div>
-                        <!-- End Main Column -->
-                        <!-- Side Column -->
-                        
-                
-                    <div class="col-lg-12 col-md-9 col-sm-12 col-xs-12">
-               
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                           Tabela de alunos
-                        </div>
-                        <div class="panel-body">
-                            <div class="table-responsive">
-                                <table class="table table-striped table-bordered table-hover">
-                                    <thead>
-                                        <tr>
-                                            <td class="col-lg-1"><strong>Matricula</strong></td>
-                                            <td class="col-lg-2"><strong>Aluno</strong></td>
-                                            <td class="col-lg-2"><strong>Curso</strong></td>
-                                            <td class="col-lg-2"><strong>Descriçao</strong></td>
-                                            <td class="col-lg-2"><strong>Data Início</strong></td>
-                                            <td class="col-lg-2"><strong>Data Término</strong></td>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                    <?php
-										// se o número de resultados for maior que zero, mostra os dados
-										if($total > 0) {
-											// inicia o loop que vai mostrar todos os dados
-											do {
-									?>
-									
-                                        <tr>
-                                            <td><?=$linha['keyword']?></td>
-                                            <td><?=$linha['address']?></td>
-                                            <td><?=$linha['course']?></td>
-                                            <td><?=$linha['description']?></td>
-                                            <td><?=$linha['data_inicio']?></td>
-                                            <td><?=$linha['data_fim']?></td>
-                                        </tr>
-                                       
-									<?php
-											// finaliza o loop que vai mostrar os dados
-											}while($linha = mysqli_fetch_assoc($dados));
-										// fim do if 
-										}
-									?>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
 
-                    </div>
-                    </div>
-                    </div>
+        <!-- Bootstrap Modals -->
+        <!-- Modal - Add New Record/User -->
+        <div class="modal fade" id="add_new_record_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Adiconar novo certificado</h4>
+              </div>
+              <div class="modal-body">
+
+                <div class="form-group">
+                  <label for="keyword">Matrícula</label>
+                  <input type="text" id="keyword" placeholder="Matrícula" class="form-control"/>
                 </div>
+
+                <div class="form-group">
+                  <label for="address">Nome</label>
+                  <input type="text" id="address" placeholder="Nome" class="form-control"/>
+                </div>
+
+                <div class="form-group">
+                  <label for="course">Curso</label>
+                  <input type="text" id="course" placeholder="Curso" class="form-control"/>
+                </div>
+
+                <div class="form-group">
+                  <label for="description">Descrição</label>
+                  <input type="text" id="description" placeholder="Descrição" class="form-control"/>
+                </div>
+
+                <div class="form-group">
+                  <label for="data_inicio">Data de início</label>
+                  <input type="text" id="data_inicio" placeholder="Data de início" class="form-control"/>
+                </div>
+
+                <div class="form-group">
+                  <label for="data_fim">Data de Término</label>
+                  <input type="text" id="data_fim" placeholder="Data de Término" class="form-control"/>
+                </div>
+
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-primary" onclick="addRecord()">Adicionar</button>
+              </div>
             </div>
-	<?php include("../php/rodape.php"); ?>
-     <!-- /. WRAPPER  -->
+          </div>
+        </div>
+        <!-- // Modal -->
+
+        <!-- Modal - Update User details -->
+        <div class="modal fade" id="update_user_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Atualizar</h4>
+              </div>
+              <div class="modal-body">
+
+
+                <div class="form-group">
+                  <label for="update_keyword">Matrícula</label>
+                  <input type="text" id="update_keyword" placeholder="Matrícula" class="form-control"/>
+                </div>
+
+                <div class="form-group">
+                  <label for="update_address">Nome</label>
+                  <input type="text" id="update_address" placeholder="Nome" class="form-control"/>
+                </div>
+
+                <div class="form-group">
+                  <label for="update_course">Curso</label>
+                  <input type="text" id="update_course" placeholder="Curso" class="form-control"/>
+                </div>
+
+                <div class="form-group">
+                  <label for="update_description">Descrição</label>
+                  <input type="text" id="update_description" placeholder="Descrição" class="form-control"/>
+                </div>
+
+                <div class="form-group">
+                  <label for="update_data_inicio">Data de início</label>
+                  <input type="text" id="update_data_inicio" placeholder="Data de início" class="form-control"/>
+                </div>
+
+                <div class="form-group">
+                  <label for="update_data_fim">Data de Término</label>
+                  <input type="text" id="update_data_fim" placeholder="Data de Término" class="form-control"/>
+                </div>
+
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-primary" onclick="UpdateUserDetails()" >Salvar</button>
+                <input type="hidden" id="hidden_user_id">
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- // Modal -->
+        
+      </div>
+    </div>
+    <?php include("../php/rodape.php"); ?>
+    <!-- /. WRAPPER  -->
     <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
-    <!-- JQUERY SCRIPTS -->
-    <script src="../assets/js/jquery-1.10.2.js"></script>
-      <!-- BOOTSTRAP SCRIPTS -->
-    <script src="../assets/js/bootstrap.min.js"></script>
-   <script type="application/javascript">
-       $('ul.main li').click(function() {
-           $(this).children('ul').toggle();
-       });
-   </script>
-   
-</body>
+    
+    <!-- Jquery JS file -->
+    <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+
+    <!-- Bootstrap JS file -->
+    <script type="text/javascript" src="bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+
+    <!-- Custom JS file -->
+    <script type="text/javascript" src="js/script.js"></script>
+    
+    <script type="application/javascript">
+      $('ul.main li').click(function() {
+        $(this).children('ul').toggle();
+      });
+    </script>
+
+  </body>
 </html>
