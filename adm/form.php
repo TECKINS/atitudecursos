1c0f3e9a34344aea9e9739dc67881cb46dc18c37
Atitude Cursos <atituderhcursos@gmail.com>
2017-12-05 13:16:18 -0300

    Update form.php

--- adm/form.php
+++ adm/form.php
@@ -4,7 +4,7 @@ session_start();
 include("../php/config.php");
 protegePaginaAdm();
 ?>
-<!DOCTYPE html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<script>
