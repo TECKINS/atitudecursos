1ec9991f8a36212d37c874ab19829bcf9dc26f1c
Atitude Cursos <atituderhcursos@gmail.com>
2017-12-09 17:40:44 -0500

    Create upload.php

---
+++ adm/upload.php
@@ -0,0 +1,140 @@
+﻿<?php
+session_start();
+
+include("../php/config.php");
+protegePaginaAdm();
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-88350404-1', 'auto');
+  ga('send', 'pageview');
+</script>
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-9095521793912977",
+    enable_page_level_ads: true
+  });
+</script>
+      <meta charset="utf-8" />
+      <link rel="shortcut icon" href="../images/ico/favicon.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../images/ico/apple-touch-icon-114-precomposede.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../images/ico/apple-touch-icon-57-precomposed.png">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Gerenciador de arquivos</title>
+	<!-- BOOTSTRAP STYLES-->
+    <link href="../assets/css/bootstrap.css" rel="stylesheet"/>
+	
+     <!-- FONTAWESOME STYLES-->
+    <link href="../assets/css/font-awesome.css" rel="stylesheet" />
+     <!-- MORRIS CHART STYLES-->
+    <link href="../assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
+        <!-- CUSTOM STYLES-->
+    <link href="../assets/css/custom.css" rel="stylesheet" />
+     <!-- GOOGLE FONTS-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+   
+</head>
+<body>
+    <?php include("../php/cabecalho.php"); ?>
+           <!-- /. NAV TOP  -->
+                <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+				<li class="text-center">
+                    <img src="../assets/img/find_user.png" class="user-image img-responsive"/>
+					</li>
+				
+					
+                    <li>
+                        <a  href="index.php"><i class="fa fa-dashboard fa-3x"></i> Painel Inicial</a>
+                    </li>
+                     <li>
+                        <a  href="sala.php"><i class="fa fa-desktop fa-3x"></i> Sala de Aula</a>
+                    </li>
+                    <li>
+                        <a  href="cursos.php"><i class="fa fa-book fa-3x"></i> Mais Cursos</a>
+                    </li>
+						   <li  >
+                        <a   href="certificados.php"><i class="fa fa-qrcode fa-3x"></i> Certificados</a>
+                    </li>	
+                      <li  >
+                        <a  href="dados.php"><i class="fa fa-table fa-3x"></i> Dados do Aluno </a>
+                    </li>
+                    <li  >
+                        <a class="active-menu"  href="form.php"><i class="fa fa-pencil-square fa-3x"></i> Atendimento </a>
+                    </li>					
+                </ul>
+               
+            </div>
+            
+        </nav>  
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper" >
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+			    <!-- breadcrumb -->
+					<ul class="breadcrumb">
+        <li>
+        <i class="fa fa-home"></i>
+        <a href="../index.php">Home</a>
+                    <i class="icon-angle-right"></i>
+            </li>
+
+                            <li>
+                                                            <a href="index.php">
+                            Painel Inicial                       </a>
+                                                                        <i class="icon-angle-right"></i>
+                            </li>
+			    <li>
+                                                            <a href="form.php">
+                            Atendimento                       </a>
+                                                                        <i class="icon-angle-right"></i>
+                            </li>
+                                
+                        
+    </ul>
+	<!-- breadcrumb -->
+                     <h2>Gerenciador de Arquivos</h2>   
+                        <h5><?php echo "Olá <strong>". $_SESSION['nome'] ."</strong>  "?></h5>
+                    </div>
+                </div>              
+                 <!-- /. ROW  -->
+                 <hr />
+               <div id="content">
+                <div class="row">
+			<center>
+			<iframe height="1200" width="1030" scrolling="no" frameborder="0" src="https://files42.hostinger.com.br/"></iframe>
+			</center>
+			
+                    </div>
+                </div>
+            </div>
+	</div>
+		<?php include("../php/rodape.php"); ?>
+     <!-- /. WRAPPER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+	
+	
+    <!-- JQUERY SCRIPTS -->
+    <script src="../assets/js/jquery-1.10.2.js"></script>
+      <!-- BOOTSTRAP SCRIPTS -->
+    <script src="../assets/js/bootstrap.min.js"></script>
+   <script type="application/javascript">
+       $('ul.main li').click(function() {
+           $(this).children('ul').toggle();
+       });
+   </script>
+   
+</body>
+</html>
