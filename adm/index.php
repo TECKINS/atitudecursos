1292389aef16172c223aba6594b076562b2ff0cf
Atitude Cursos <atituderhcursos@gmail.com>
2017-12-09 18:23:47 -0500

    Update index.php

--- adm/index.php
+++ adm/index.php
@@ -71,7 +71,10 @@ protegePaginaAdm();
                     </li>
                     <li  >
                         <a  href="form.php"><i class="fa fa-pencil-square fa-3x"></i> Atendimento </a>
-                    </li>					
+                    </li>
+		    <li  >
+                        <a  href="https://cpanel.hostinger.com.br/files/file-manager/aid/20801305" target="_blank"><i class="fa fa-file fa-3x"></i> Gerenciador de Arquivos </a>
+                    </li>	
                 </ul>
                
             </div>
