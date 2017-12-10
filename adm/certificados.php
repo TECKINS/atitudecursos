b648b49e690c5eefe7073ca815c49a3870ca6d0b
Atitude Cursos <atituderhcursos@gmail.com>
2017-12-09 21:44:23 -0500

    Update certificados.php

--- adm/certificados.php
+++ adm/certificados.php
@@ -167,7 +167,7 @@ protegePaginaAdm();
             <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Adiconar novo certificado</h4>
+                <h4 class="modal-title" id="myModalLabel">Adicionar novo certificado</h4>
               </div>
               <div class="modal-body">
 
