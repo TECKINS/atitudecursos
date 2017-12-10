e1a72dd8180a23598d2e860ebbe14788ac27150a
Atitude Cursos <atituderhcursos@gmail.com>
2017-12-09 19:50:05 -0500

    Update form.php

--- adm/form.php
+++ adm/form.php
@@ -76,6 +76,9 @@ protegePaginaAdm();
 		    <li>
 			<a  href="https://files42.hostinger.com.br/" target="_blank"><i class="fa fa-file fa-3x"></i> Gerenciador de Arquivos </a>
 		    </li>
+			<li>
+                        <a  href="https://webmail1.hostinger.com.br/roundcube/" target="_blank"><i class="fa fa-envelope fa-3x"></i> Email </a>
+                    </li>
                 </ul>
                
             </div>
