8c11fd5bfc3422a9e5bf3a9a7a7c018c2898c7fa
Atitude Cursos <atituderhcursos@gmail.com>
2017-12-23 22:53:54 -0200

    Update sala.php

--- adm/sala.php
+++ adm/sala.php
@@ -45,12 +45,7 @@ protegePaginaAdm();
            <!-- /. NAV TOP  -->
                 <nav class="navbar-default navbar-side" role="navigation">
             <div class="sidebar-collapse">
-                <ul class="nav" id="main-menu">
-				<li class="text-center">
-                    <img src="../assets/img/find_user.png" class="user-image img-responsive"/>
-					</li>
-				
-					
+                <ul class="nav" id="main-menu">		
                     <li>
                         <a  href="index.php"><i class="fa fa-dashboard fa-3x"></i> Painel Inicial</a>
                     </li>
