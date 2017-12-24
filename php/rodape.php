ef6c303373c99835e3c95eedbf849046c3e96286
Atitude Cursos <atituderhcursos@gmail.com>
2017-12-24 00:06:46 -0200

    Update rodape.php

--- php/rodape.php
+++ php/rodape.php
@@ -2,12 +2,14 @@
   <div class="footer" style="background: #202020; padding: 70px 0; padding-bottom: 30px;"> 
     <div class="container"> 
       <div class="row"> 
+        <div class="col-lg-2 col-md-4 col-sm-12 col-xs-12 md-margin-bottom-40 text-justify"> 
+        </div>
         <!-- About --> <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12 md-margin-bottom-40 text-justify"> 
         <div class="headline">
           <h2 style="color: #ffffff; font-weight: 600;">Atitude Cursos</h2></div> 
         <p style="color: #ffffff;">Nós da Atitude Cursos somos uma empresa que temos como missão levar educação profissionalizante de qualidade aos quatro cantos do brasil e do mundo por um preço justo.</p>
         <p style="color: #ffffff;">Somos associados a Associação Brasileira de Educação a Distância (ABED) e temos nossa atuação Autorizada de acordo com a Lei nº 9.394/96 e Decreto Presidencial nº 5.154/04.</p> </div> 
-        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12"> 
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"> 
           <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 md-margin-bottom-40"> 
           <div class="headline text-center">
             <h2 style="color: #ffffff; font-weight: 600;">Institucional</h2></div> </div> 
