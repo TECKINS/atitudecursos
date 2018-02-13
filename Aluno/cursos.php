ce89bd56e79b7621f4d9e74ed2bf1268cb5854f0
Atitude Cursos <atituderhcursos@gmail.com>
2018-02-13 10:14:08 -0200

    Update cursos.php

--- Aluno/cursos.php
+++ Aluno/cursos.php
@@ -46,38 +46,7 @@ protegePagina();
 </head>
 <body>
 	<?php include("../php/cabecalho.php"); ?>
-	<div id="sidebar-wrapper">
-		<!-- /. NAV TOP  -->
-		<nav class="navbar-default navbar-side" role="navigation">
-			<div class="sidebar-collapse">
-				<ul class="nav" id="main-menu">
-					<li class="text-center">
-						<img src="../assets/img/find_user.png" class="user-image img-responsive"/>
-					</li>
-					<li>
-						<a href="index.php"><i class="fa fa-dashboard fa-3x"></i> Painel Inicial</a>
-					</li>
-					<li>
-						<a  href="sala.php"><i class="fa fa-desktop fa-3x"></i> Sala de Aula</a>
-					</li>
-					<li>
-						<a class="active-menu"  href="cursos.php"><i class="fa fa-book fa-3x"></i> Mais Cursos</a>
-					</li>
-					<li  >
-						<a   href="certificados.php"><i class="fa fa-qrcode fa-3x"></i> Certificados</a>
-					</li>	
-					<li  >
-						<a  href="dados.php"><i class="fa fa-table fa-3x"></i> Dados do Aluno </a>
-					</li>
-					<li  >
-						<a  href="form.php"><i class="fa fa-pencil-square fa-3x"></i> Atendimento </a>
-					</li>					
-				</ul>
-
-			</div>
-
-		</nav>  
-	</div>
+	<?php include("menu.php"); ?>
 		<!-- /. NAV SIDE  -->
 		<div id="page-wrapper" >
 			<div id="page-inner">
