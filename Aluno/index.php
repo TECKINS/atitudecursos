107a3e74b7d14d45a9931e7bf62f2e0260682107
Atitude Cursos <atituderhcursos@gmail.com>
2018-02-13 10:17:31 -0200

    Update index.php

--- Aluno/index.php
+++ Aluno/index.php
@@ -52,39 +52,8 @@ protegePagina();
 </head>
 <body>
 	<?php include("../php/cabecalho.php"); ?>
-		<div id="sidebar-wrapper">
-		   <!-- /. NAV TOP  -->
-			<nav class="navbar-default navbar-side" role="navigation">
-				<div class="sidebar-collapse">
-					<ul class="nav" id="main-menu">
-						<li class="text-center">
-							<img id="icone" src="../assets/img/find_user.png" class="user-image img-responsive" width="128px" height="128px"/>
-						</li>
-						<li onmouseover="mudaFoto('../images/painelinicial.png')" onmouseout="mudaFoto('../assets/img/find_user.png')">
-							<a class="active-menu"  href="index.php"><i class="fa fa-dashboard fa-3x"></i> Painel Inicial</a>
-						</li>
-						<li onmouseover="mudaFoto('../images/saladeaula.png')" onmouseout="mudaFoto('../assets/img/find_user.png')">
-							<a  href="sala.php"><i class="fa fa-desktop fa-3x"></i> Sala de Aula</a>
-						</li>
-						<li onmouseover="mudaFoto('../images/maiscursos.png')" onmouseout="mudaFoto('../assets/img/find_user.png')">
-							<a  href="cursos.php"><i class="fa fa-book fa-3x"></i> Mais Cursos</a>
-						</li onmouseover="mudaFoto('../images/maiscursos.png')" onmouseout="mudaFoto('../assets/img/find_user.png')">
-						<li onmouseover="mudaFoto('../images/certificacao.png')" onmouseout="mudaFoto('../assets/img/find_user.png')">
-							<a   href="certificados.php"><i class="fa fa-qrcode fa-3x"></i> Certificados</a>
-						</li>
-						<li onmouseover="mudaFoto('../images/dadosdoaluno.png')" onmouseout="mudaFoto('../assets/img/find_user.png')">
-							<a  href="dados.php"><i class="fa fa-table fa-3x"></i> Dados do Aluno </a>
-						</li>
-						<li onmouseover="mudaFoto('../images/atendimento.png')" onmouseout="mudaFoto('../assets/img/find_user.png')">
-							<a  href="form.php"><i class="fa fa-pencil-square fa-3x"></i> Atendimento </a>
-						</li>
-					</ul>
-
-				</div>
-
-			</nav>
-		</div>
-		<div id="page-content-wrapper">
+	<?php include("menu.php"); ?>
+	<div id="page-content-wrapper">
 			<!-- /. NAV SIDE  -->
 			<div id="page-wrapper" >
 				<div id="page-inner">
