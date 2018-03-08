7cda32d3c21a60cf49bcc3e9fedeb65b04b27822
Atitude Cursos <atituderhcursos@gmail.com>
2018-03-08 01:17:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -299,6 +299,8 @@ include("php/config.php");
                                     
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
     <script src="js/main.js"></script>
                                     
     <script>
