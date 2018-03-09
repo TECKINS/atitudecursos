bbed0ce7f5afc42431f217e4e1f870400707d942
Atitude Cursos <atituderhcursos@gmail.com>
2018-03-09 15:59:04 -0300

    Update altera.php

--- php/altera.php
+++ php/altera.php
@@ -1,47 +1,55 @@
-﻿<?php
+<?php
 
 session_start();  // Inicia a session
 
 include "config.php";
 
-$nome =$_POST['nome'];
+$nome = $_POST['nome'];
 $email = $_SESSION['email'];
-$rg =$_POST['rg'];
-$cpf =$_POST['cpf'];
-$pais =$_POST['pais'];
-$endereco =$_POST['endereco'];
-$numero =$_POST['numero'];
-$complemento =$_POST['complemento'];
-$bairro =$_POST['bairro'];
-$cep =$_POST['cep'];
-$cidade =$_POST['cidade'];
-$uf =$_POST['uf'];
-$telefone =$_POST['telefone'];
-$senha =($_POST['senha']);
-$c_senha =($_POST['c_senha']);
+$rg = $_POST['rg'];
+$cpf = $_POST['cpf'];
+$pais = $_POST['pais'];
+$endereco = $_POST['endereco'];
+$numero = $_POST['numero'];
+$complemento = $_POST['complemento'];
+$bairro = $_POST['bairro'];
+$cep = $_POST['cep'];
+$cidade = $_POST['cidade'];
+$uf = $_POST['uf'];
+$telefone = $_POST['telefone'];
+$senha = ($_POST['senha']);
+$c_senha = ($_POST['c_senha']);
+$img = $_FILE['img'];
 
-if($nome == '' || $rg == '' || $cpf == '' || $pais == '' || $endereco == '' || $numero == '' || $complemento == '' || $bairro == '' || $cep == '' || $cidade == '' || $uf == '' || $telefone == '' ) {  
-		echo("<script>alert('Nenhum dos campos podem ficar em branco. Verifique e tente novamente!'); location.href='../Aluno/dados.php';</script>");
+if($nome == '' || $rg == '' || $cpf == '' || $pais == '' || $endereco == '' || $numero == '' || $complemento == '' || $bairro == '' || $cep == '' || $cidade == '' || $uf == '' || $telefone == '') {  
+	echo("<script>alert('Nenhum dos campos podem ficar em branco. Verifique e tente novamente!'); location.href='../Aluno/dados.php';</script>");
 } else {
 		//if ($senha == '' || $c_senha == '') {
 			// Inserindo os dados no banco de dados
 
-				$sql = mysqli_query($conexao, "UPDATE aluno SET nome='{$nome}', rg='{$rg}', cpf='{$cpf}', pais='{$pais}', endereco='{$endereco}', numero='{$numero}', complemento='{$complemento}', bairro='{$bairro}', cep='{$cep}', cidade='{$cidade}', uf='{$uf}', telefone='{$telefone}' WHERE email = '$email'") 
-									or die( mysql_error());
-				if($senha != '' || $c_senha != ''){
-						if ($senha == $c_senha) {
-							$sql = mysqli_query($conexao, "UPDATE aluno SET senha=sha1('{$senha}') WHERE email = '$email'") 
-				
-									or die( mysql_error() );
-									header("Location:../Aluno/dados.php");
-								
-						} else {
-							echo("<script>alert('Confirmação de senha incorreta! Por favor verifique e tente novamente.'); location.href='../Aluno/dados.php';</script>");	
-						}
-				} else {
-					header("Location:../Aluno/dados.php");
-				}
-										
+	$sql = mysqli_query($conexao, "UPDATE aluno SET nome='{$nome}', rg='{$rg}', cpf='{$cpf}', pais='{$pais}', endereco='{$endereco}', numero='{$numero}', complemento='{$complemento}', bairro='{$bairro}', cep='{$cep}', cidade='{$cidade}', uf='{$uf}', telefone='{$telefone}' WHERE email = '$email'") 
+	or die( mysql_error());
+
+	if (move_uploaded_file($img['tmp_name'], "../assets/".$img['name'])) {
+		echo "Upload efetuado com sucesso!";
+	} else {
+		echo "Não foi possível enviar o arquivo, tente novamente";
+	}
+
+	if($senha != '' || $c_senha != ''){
+		if ($senha == $c_senha) {
+			$sql = mysqli_query($conexao, "UPDATE aluno SET senha=sha1('{$senha}') WHERE email = '$email'") 
+
+			or die( mysql_error() );
+			header("Location:../Aluno/dados.php");
+
+		} else {
+			echo("<script>alert('Confirmação de senha incorreta! Por favor verifique e tente novamente.'); location.href='../Aluno/dados.php';</script>");	
+		}
+	} else {
+		header("Location:../Aluno/dados.php");
+	}
+
 		/*} else {
 			if ($senha == $c_senha) {
            
@@ -58,9 +66,8 @@ if($nome == '' || $rg == '' || $cpf == '' || $pais == '' || $endereco == '' || $
 			echo("<script>alert('Confirmação de senha incorreta! Por favor verifique e tente novamente.'); location.href='../Aluno/dados.php';</script>");
 	
 		}	*/
-		}
+	}
 
-		
+
 //}
-
 ?>
