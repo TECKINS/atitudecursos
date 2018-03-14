a548ed744b75709d3b775511deccbf715ed9ff27
Atitude Cursos <atituderhcursos@gmail.com>
2018-03-14 16:28:06 -0300

    Add carregaPag

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,13 @@
+function carregaPag(pagina) {
+    $.ajax({
+        url: 'paginas/'+pagina+'.php',
+        type: 'post',
+    })
+    .done(function(retorno) {
+        $('#page-inner').html(retorno);
+    });
+}
+
 $(document).ready(function () {
     $('#menu-toggle').click(function(event) {
         $('.sidebar-menu').toggleClass('active');
