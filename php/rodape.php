2b23d77e1573163af54db9683cfe8f2685680985
Atitude Cursos <atituderhcursos@gmail.com>
2018-03-14 15:43:43 -0300

    Update  rodape.php

--- php/rodape.php
+++ php/rodape.php
@@ -1,71 +1,72 @@
 <div class="footer" id="footer">
-		<div class="container">
-			<div class="row"> 
-				<!-- About -->
-				<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 md-margin-bottom-40 text-justify">
-					<div class="headline">
-						<h2 style="color: #ffffff; font-weight: 600;">Atitude Cursos</h2></div>
-						<p style="color: #ffffff;">Nós da Atitude Cursos somos uma empresa que temos como missão levar educação profissionalizante de qualidade aos quatro cantos do brasil e do mundo por um preço justo.</p>
-						<p style="color: #ffffff;">Somos associados a Associação Brasileira de Educação a Distância (ABED) e temos nossa atuação Autorizada de acordo com a Lei nº 9.394/96 e Decreto Presidencial nº 5.154/04.</p>
+	<div class="container">
+		<div class="row"> 
+			<!-- About -->
+			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 md-margin-bottom-40 text-justify">
+				<div class="headline">
+					<h2 style="color: #ffffff; font-weight: 600;">Atitude Cursos</h2></div>
+					<p style="color: #ffffff;">Nós da Atitude Cursos somos uma empresa que temos como missão levar educação profissionalizante de qualidade aos quatro cantos do brasil e do mundo por um preço justo.</p>
+					<p style="color: #ffffff;">Somos associados a Associação Brasileira de Educação a Distância (ABED) e temos nossa atuação Autorizada de acordo com a Lei nº 9.394/96 e Decreto Presidencial nº 5.154/04.</p>
 				</div>
 				<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
 					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 md-margin-bottom-40">
 						<div class="headline text-center">
 							<h2 style="color: #ffffff; font-weight: 600;">Institucional</h2></div>
-					</div>
-					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 md-margin-bottom-40">
-						<ul class="list-unstyled link-list">
-							<li class="padding-bottom-5 padding-top-5">
-								<a href="#" title="Autentique aqui o registro do seu certificado">Autenticar Certificado</a></li>
-								<li class="padding-bottom-5 padding-top-5"><a href="#" title="Pacotes de cursos especiais para empresas">Para Empresas</a></li>
-								<li class="padding-bottom-5 padding-top-5"><a href="#" title="Venha fazer parte de nossa equipe de colaboradores">Seja um Instrutor Atitude Cursos</a></li>
-								<li class="padding-bottom-5 padding-top-5" ><a href="#" title="Conheça nosso blog com várias dicas de estudos">Atitude Cursos Blog</a></li>
-								<li class="padding-bottom-5 padding-top-5"><a href="#" title="Conheça nossos mais de 2 mil cursos disponíveis">Lista de Cursos</a></li>
-						</ul>
-					</div><!--/col-md-3-->
-					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 md-margin-bottom-40">
-						<ul class="list-unstyled link-list">
-							<li class="padding-bottom-5 padding-top-5">
-								<a href="#" title="Conheça os locais onde pode usar seu Certificado">10 locais para usar o Certificado</a></li>
-								<li class="padding-bottom-5 padding-top-5"><a href="#" title="Veja como aplicar seu Certificado em Educação Continuada">Educação Continuada</a></li>
-								<li class="padding-bottom-5 padding-top-5"><a href="#" title="Conheça do 5 principais motivos para fazer um Curso Online">5 Motivos para fazer um Curso Online</a></li>
-								<li class="padding-bottom-5 padding-top-5"><a href="#" title="Você pode apoiar nosso site fazendo uma doação.">Faça uma Doação</a></li> <li class="padding-bottom-5 padding-top-5"><a href="#" title="Conheça do Modelo do seu Certificado.">Modelo do Certificado</a></li>
-						</ul>
-					</div><!--/col-md-3-->
-					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 md-margin-bottom-40 text-center">
-						<span style="font-size: 24px; color: #ffffff;"><i class="fa fa-phone">&nbsp;</i>(91)&nbsp;3754-2307</span><br>
-						<span style="font-size: 24px; color: #ffffff;"><i class="fa fa-whatsapp">&nbsp;</i>(91)&nbsp;98717-2307</span><br>
-						<span style="font-size: 14px; color: #ffffff;">Segunda a Sexta de 08:00 hs as 21:00 hs e Sábado de 09:00 hs as 17:00 hs</span>
-					</div>
-				</div> <!-- End Latest --> <!-- Address -->
-				<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 md-margin-bottom-40 endereco">
-					<div class="headline">
-						<h2 style="color: #ffffff; font-weight: 600;">Nossos Contatos</h2></div>
-						<address class="md-margin-bottom-40" style="color: #ffffff;">
-							<span style="font-size: 18px; color: #ffffff;">
-								<b>Atitude Cursos<br>CNPJ: 18.349.208/0001-86</b>
-							</span> <br><br>
-							<span>Rua Germano Aranha, Qd. 294 Lt. 03 - Vila dos Cabanos - Barcarena-PA CEP: 68.447-000</span> <br><br>
-							<span style="color: #ffffff;"> Email: atendimento@atitudecursos.org</span>
-						</address>
-						<ul class="footer-socials list-inline" style="font-size: 24px">
-							<li>
-								<a href="http://www.facebook.com/share.php?u=http://www.atitudecursos.org" target="_blank" class="tooltips" data-toggle="tooltip" data-placement="top" title="Facebook Atitude Cursos" data-original-title="Facebook">
-								<i class="fa fa-facebook"></i> </a>
-							</li>
-							<li>
-								<a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Google Plus">
-								<i class="fa fa-google-plus"></i> </a>
-							</li>
-						</ul>
-						<a class="btn btn-primary btn-lg" href="form.php">Fale Conosco</a>
-				</div><!--/col-md-3--> <!-- End Address -->
-			</div>
-	</div><!--/footer-->
-			<div class="copyright" style="text-align: center; color: #ffffff; background: #202020;">
-				<div class="container">
-					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-						<p> <?php echo date('Y', time()) ?> © Atitude Cursos. Todos os Direitos Reservados. <a href="/politica-de-privacidade">Política de Privacidade</a> | <a href="/termos-de-uso">Termos de Uso</a> </p>
-					</div>
-				</div>
-			</div><!--/copyright-->
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 md-margin-bottom-40">
+							<ul class="list-unstyled link-list">
+								<li class="padding-bottom-5 padding-top-5">
+									<a href="#" title="Autentique aqui o registro do seu certificado">Autenticar Certificado</a></li>
+									<li class="padding-bottom-5 padding-top-5"><a href="#" title="Pacotes de cursos especiais para empresas">Para Empresas</a></li>
+									<li class="padding-bottom-5 padding-top-5"><a href="#" title="Venha fazer parte de nossa equipe de colaboradores">Seja um Instrutor Atitude Cursos</a></li>
+									<li class="padding-bottom-5 padding-top-5" ><a href="#" title="Conheça nosso blog com várias dicas de estudos">Atitude Cursos Blog</a></li>
+									<li class="padding-bottom-5 padding-top-5"><a href="#" title="Conheça nossos mais de 2 mil cursos disponíveis">Lista de Cursos</a></li>
+								</ul>
+							</div><!--/col-md-3-->
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 md-margin-bottom-40">
+								<ul class="list-unstyled link-list">
+									<li class="padding-bottom-5 padding-top-5">
+										<a href="#" title="Conheça os locais onde pode usar seu Certificado">10 locais para usar o Certificado</a></li>
+										<li class="padding-bottom-5 padding-top-5"><a href="#" title="Veja como aplicar seu Certificado em Educação Continuada">Educação Continuada</a></li>
+										<li class="padding-bottom-5 padding-top-5"><a href="#" title="Conheça do 5 principais motivos para fazer um Curso Online">5 Motivos para fazer um Curso Online</a></li>
+										<li class="padding-bottom-5 padding-top-5"><a href="#" title="Você pode apoiar nosso site fazendo uma doação.">Faça uma Doação</a></li> <li class="padding-bottom-5 padding-top-5"><a href="#" title="Conheça do Modelo do seu Certificado.">Modelo do Certificado</a></li>
+									</ul>
+								</div><!--/col-md-3-->
+								<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 md-margin-bottom-40 text-center">
+									<span style="font-size: 24px; color: #ffffff;"><i class="fa fa-phone">&nbsp;</i>(91)&nbsp;3754-2307</span><br>
+									<span style="font-size: 24px; color: #ffffff;"><i class="fa fa-whatsapp">&nbsp;</i>(91)&nbsp;98717-2307</span><br>
+									<span style="font-size: 14px; color: #ffffff;">Segunda a Sexta de 08:00 hs as 21:00 hs e Sábado de 09:00 hs as 17:00 hs</span>
+								</div>
+							</div> <!-- End Latest --> <!-- Address -->
+							<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 md-margin-bottom-40 endereco">
+								<div class="headline">
+									<h2 style="color: #ffffff; font-weight: 600;">Nossos Contatos</h2></div>
+									<address class="md-margin-bottom-40" style="color: #ffffff;">
+										<span style="font-size: 18px; color: #ffffff;">
+											<b>Atitude Cursos<br>CNPJ: 18.349.208/0001-86</b>
+										</span> <br><br>
+										<span>Rua Germano Aranha, Qd. 294 Lt. 03 - Vila dos Cabanos - Barcarena-PA CEP: 68.447-000</span> <br><br>
+										<span style="color: #ffffff;"> Email: atendimento@atitudecursos.org</span>
+									</address>
+									<ul class="footer-socials list-inline" style="font-size: 24px">
+										<li>
+											<a href="http://www.facebook.com/share.php?u=http://www.atitudecursos.org" target="_blank" class="tooltips" data-toggle="tooltip" data-placement="top" title="Facebook Atitude Cursos" data-original-title="Facebook">
+												<i class="fa fa-facebook"></i> </a>
+											</li>
+											<li>
+												<a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Google Plus">
+													<i class="fa fa-google-plus"></i> </a>
+												</li>
+											</ul>
+											<a class="btn btn-primary btn-lg" href="form.php">Fale Conosco</a>
+										</div><!--/col-md-3--> <!-- End Address -->
+									</div>
+								</div>
+							</div><!--/footer-->
+							<div class="copyright" style="text-align: center; color: #ffffff; background: #202020;">
+								<div class="container">
+									<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+										<p> <?php echo date('Y', time()) ?> © Atitude Cursos. Todos os Direitos Reservados. <a href="/politica-de-privacidade">Política de Privacidade</a> | <a href="/termos-de-uso">Termos de Uso</a> </p>
+									</div>
+								</div>
+							</div><!--/copyright-->
